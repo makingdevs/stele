3c3cc2f1ae9de94b8d49e420fc4aa93c234ec39e
neodevelop <neodevelop@gmail.com>
2013-02-05 11:41:25 -0600

    Adding ruby base files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stele::Application.config.secret_token = '9055b9d63c000606e9c0bcb0dbbf24164a0667e79a32737f0167a3c4b92935963aa2733658bab30d5adb81b10fdd2d1076595405cad2caf27052e01d32af506c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Stele::Application.config.session_store :cookie_store, key: '_stele_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Stele::Application.config.session_store :active_record_store
