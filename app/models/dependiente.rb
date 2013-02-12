54c3016efddcf2f50c31f8e070781a4e86232120
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-02-11 19:46:12 -0600

    coding #4 agregando relaciones entre dependiente y usuario

--- app/models/dependiente.rb
+++ app/models/dependiente.rb
@@ -2,4 +2,5 @@ class Dependiente < ActiveRecord::Base
   attr_accessible :perfil_id
 
   belongs_to :perfil
+  has_and_belongs_to_many :usuarios
 end
--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,4 +2,5 @@ class Usuario < ActiveRecord::Base
   attr_accessible :email, :password, :perfil_id
 
   belongs_to :perfil
+  has_and_belongs_to_many :dependientes
 end
