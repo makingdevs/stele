327de06a7e06d90de29d19d6f4ce995d3bc3f441
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-02-17 21:29:59 -0600

    coding #10 regenerando la vista para perfiles y eliminando los controllers generados para autoridades, dependientes

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,5 +1,6 @@
 class Usuario < ActiveRecord::Base
-  attr_accessible :email, :password, :perfil_id
+  attr_accessible :email, :password
+  attr_protected :perfil_id
 
   belongs_to :perfil
   has_and_belongs_to_many :dependientes
--- test/functional/perfils_controller_test.rb
+++ test/functional/perfils_controller_test.rb
@@ -1,49 +1,7 @@
 require 'test_helper'
 
 class PerfilsControllerTest < ActionController::TestCase
-  setup do
-    @perfil = perfils(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:perfils)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create perfil" do
-    assert_difference('Perfil.count') do
-      post :create, perfil: { apellidoMaterno: @perfil.apellidoMaterno, apellidoPaterno: @perfil.apellidoPaterno, curp: @perfil.curp, fechaDeNacimiento: @perfil.fechaDeNacimiento, genero: @perfil.genero, nombre: @perfil.nombre }
-    end
-
-    assert_redirected_to perfil_path(assigns(:perfil))
-  end
-
-  test "should show perfil" do
-    get :show, id: @perfil
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @perfil
-    assert_response :success
-  end
-
-  test "should update perfil" do
-    put :update, id: @perfil, perfil: { apellidoMaterno: @perfil.apellidoMaterno, apellidoPaterno: @perfil.apellidoPaterno, curp: @perfil.curp, fechaDeNacimiento: @perfil.fechaDeNacimiento, genero: @perfil.genero, nombre: @perfil.nombre }
-    assert_redirected_to perfil_path(assigns(:perfil))
-  end
-
-  test "should destroy perfil" do
-    assert_difference('Perfil.count', -1) do
-      delete :destroy, id: @perfil
-    end
-
-    assert_redirected_to perfils_path
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
