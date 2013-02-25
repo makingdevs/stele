2da5c9cc96f45f4759da798c6e665a66f68bd3cf
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-02-24 23:02:19 -0600

    coding #12 agregando relación faltante

--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -2,4 +2,5 @@ class Perfil < ActiveRecord::Base
   attr_accessible :apellidoMaterno, :apellidoPaterno, :curp, :fechaDeNacimiento, :genero, :nombre
 
   has_one :usuario
+  has_one :dependiente
 end
