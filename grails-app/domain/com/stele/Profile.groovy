8f3946ad8ac4cfeeae3bdb490790f35d4b08955b
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-03-06 21:19:06 -0600

    coding #2 agregando la columna de sexo al profile

--- grails-app/domain/com/stele/Profile.groovy
+++ grails-app/domain/com/stele/Profile.groovy
@@ -7,11 +7,17 @@ class Profile {
   String apellidoMaterno
   String curp
   Date fechaDeNacimiento
+  Sexo sexo
 
   Date dateCreated
   Date lastUpdated
 
   static constraints = {
-    apellidoMaterno blank: true, nullable: true
+    nombre blank:false, size:1..100
+    apellidoPaterno blank:false, size:1..100
+    apellidoMaterno nullable:true, size:1..100
+    fechaDeNacimiento nullable:true
+    sexo()
+    curp size:1..18
   }
 }
