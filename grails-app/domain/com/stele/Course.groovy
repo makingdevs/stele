b0f8799882a3313dc07b7a4bec3b9c797f501c1c
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-03-10 11:14:55 -0600

    coding #3 cambiando la columna group por backet ya que la primera es palabra reservada

--- grails-app/domain/com/stele/Course.groovy
+++ grails-app/domain/com/stele/Course.groovy
@@ -5,15 +5,17 @@ class Course {
   Institute institute
   Degree degree
   
-  String group
+  String bracket
   String year
 
   static belongsTo = Dependent
   static hasMany = [dependents : Dependent]
 
-  static constraints = {
-    group size: 1..10
-    year: 1..2
-  }
+  Date dateCreated
+  Date lastUpdated
 
+  static constraints = {
+    bracket size: 1..10
+    year size: 1..2
+  }
 }
