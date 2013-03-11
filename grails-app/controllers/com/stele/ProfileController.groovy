7f475ca535475cce2de34d81b0c626005b190328
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-03-11 00:16:43 -0600

    coding #10 creando la acción para guardar el perfil del usuario y actualizarlo

--- grails-app/controllers/com/stele/ProfileController.groovy
+++ grails-app/controllers/com/stele/ProfileController.groovy
@@ -4,4 +4,14 @@ class ProfileController {
 
   def scaffold = Profile
 
+  def savingUserProfile() {
+    Profile profile = new Profile(params)
+    profile.save()
+    
+    User user = User.get(params.userId)
+    user.profile = profile
+    user.save()
+    user
+  }
+
 }
