772acd5740942cca08b469b73b73f175deabfa36
neodevelop <your@gmail.com>
2013-03-18 13:48:05 -0600

    Coding #13 agregando columnas a telefono y restricciones

--- grails-app/domain/com/stele/Telefono.groovy
+++ grails-app/domain/com/stele/Telefono.groovy
@@ -2,6 +2,12 @@ package com.stele
 
 class Telefono {
 
+  String numeroTelefonico
+  TipoDeTelefono tipoDeTelefono
+  String extension
+
   static constraints = {
+    numeroTelefonico size:1..10,blank:false
+    extension size:1..6,nullable:true,blank:true
   }
 }
