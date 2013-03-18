88d0ead4b880a42928857079299cd60c20a00032
neodevelop <your@gmail.com>
2013-03-18 13:55:04 -0600

    Coding #13 agregando columna sexo a clase y restricciones

--- grails-app/domain/com/stele/Perfil.groovy
+++ grails-app/domain/com/stele/Perfil.groovy
@@ -6,9 +6,13 @@ class Perfil {
   String apellidoPaterno
   String apellindoMaterno
   Date fechaDeNacimiento
+  Sexo sexo
 
   static hasMany = [direccion: Direccion, telefonos: Telefono]
 
   static constraints = {
+    nombre size:1..50,blank:false
+    apellidoPaterno size:1..50,blank:false
+    apellindoMaterno size:1..50,blank:true,nullable:true
   }
 }
