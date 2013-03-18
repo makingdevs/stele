dbbe34e2aad7a01fb57be7942b37b46d546128c0
neodevelop <your@gmail.com>
2013-03-18 13:16:07 -0600

    Coding #13 agregando clase de dominio direccion

---
+++ grails-app/domain/com/stele/Direccion.groovy
@@ -0,0 +1,20 @@
+package com.stele
+
+class Direccion {
+  String calle
+  String colonia
+  String codigoPostal
+  String delegacionMunicipio
+  String numeroInterior
+  String numeroExterior
+  String estado
+  String ciudad
+  static constraints = {
+    calle size:1..100,blank:false
+    delegacionMunicipio size:1..100,blank:false
+    colonia size:1..100,blank:false
+    codigoPostal size:6..6,blank:false
+    ciudad nullable:true
+    numeroExterior size:1..100,blank:false
+  }
+}
