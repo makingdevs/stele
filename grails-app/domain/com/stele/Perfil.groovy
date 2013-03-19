620f7a9edbe1959ced4d5997728fa8778dd90203
neodevelop <your@gmail.com>
2013-03-18 18:22:29 -0600

    coding #13 arreglando detalles en las relaciones de dominio

--- grails-app/domain/com/stele/Perfil.groovy
+++ grails-app/domain/com/stele/Perfil.groovy
@@ -8,6 +8,7 @@ class Perfil {
   Date fechaDeNacimiento
   Sexo sexo = Sexo.QUIMERA
 
+  static hasOne = [dependiente : Dependiente]
   static hasMany = [direccion: Direccion, telefonos: Telefono]
 
   Date dateCreated
