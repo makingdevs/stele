e87dc21e4119a1e8ff4feabae0483ad8b2eaba32
neodevelop <your@gmail.com>
2013-03-25 12:39:18 -0600

    Coding #21 refactor UsuarioServiceSpec

--- test/unit/com/stele/UsuarioServiceSpec.groovy
+++ test/unit/com/stele/UsuarioServiceSpec.groovy
@@ -12,32 +12,41 @@ class UsuarioServiceSpec  extends Specification{
 
     def "Obtener un usuario apartir de un command leeido desde un excel"(){
       given:
-        def filaExcelCommand = new FilaExcelCommand(
-          cicloEscolar: "2021089",
-          tutorNombre: "Rodrigo",
-          tutorApellidoPaterno: "Martínez",
-          tutorApellidoMaterno: "García",
-          correoElectronico: "rockdrigo.mtz@gmail.com",
-          telefono: "1234567",
-          matricula: "765434567",
-          dependienteApellidoPaterno: "Juan",
-          dependienteApellidoMaterno: "Perez",
-          dependienteNombre: "Perez",
-          nivel: "Primaria",
-          grado: "1",
-          grupo: "A") 
+        def filaExcelCommand = new FilaExcelCommand(datosBasicos) 
       when:
         def usuario = service.obtenerUsuarioDesdeCommand(filaExcelCommand)
       then:
-        assert usuario.username == "rockdrigo.mtz@gmail.com"
+        assert usuario.username == usernameEsperado
         assert usuario.password.size() > 0
         assert usuario.enabled
         assert !usuario.accountExpired
         assert !usuario.accountLocked
         assert !usuario.passwordExpired
-        assert usuario.perfil.nombre == "Rodrigo"
-        assert usuario.perfil.apellidoPaterno == "Martínez"
-        assert usuario.perfil.apellidoMaterno == "García"
+        assert usuario.perfil.nombre == nombreEsperado
+        assert usuario.perfil.apellidoPaterno == apellidoPaternoEsperado
+        assert usuario.perfil.apellidoMaterno == apellidoMaternoEsperado
+      where:
+        datosBasicos << [
+          [tutorNombre:"Rodrigo",tutorApellidoPaterno:"Martínez",tutorApellidoMaterno:"García",correoElectronico:"rockdrigo.mtz@gmail.com"],
+          [tutorNombre:"Juan",tutorApellidoPaterno:"Perez",tutorApellidoMaterno:"Perez",correoElectronico:"j.perez@gmail.com"]
+        ]
+        nombreEsperado << [
+          "Rodrigo",
+          "Juan"
+        ]
+        apellidoPaternoEsperado << [
+          "Martínez",
+          "Perez"
+        ]
+        apellidoMaternoEsperado << [
+          "García",
+          "Perez"
+        ]
+        usernameEsperado << [
+          "rockdrigo.mtz@gmail.com",
+          "j.perez@gmail.com"
+        ]
+
 
     }
 
