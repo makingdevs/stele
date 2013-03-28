ba1b5201fa3beba8624f2960f661ffb687b40f76
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-03-27 23:54:00 -0600

    coding #31 dejando propuesta de como cambiar la creación del mapa

--- grails-app/services/com/stele/EstructuraInstitucionalService.groovy
+++ grails-app/services/com/stele/EstructuraInstitucionalService.groovy
@@ -11,34 +11,20 @@ class EstructuraInstitucionalService {
       }
 
       if(cicloEscolarEncontrado) {
-        log.debug "cicloEscolarEncontrado : $cicloEscolarEncontrado"
-        log.debug "clave : ${filaDominio.cicloEscolar.clave}"
-
         log.debug cicloEscolarEncontrado."2011-12"
-
-
         def nivelEstudioEncontrado = cicloEscolarEncontrado."${filaDominio.cicloEscolar.clave}".find { k, v ->
-          log.debug "k : $k"
-          log.debug "filaDominio.distribucionInstitucional.nivelDeEstudio.key : ${filaDominio.distribucionInstitucional.nivelDeEstudio.key}"
           k == filaDominio.distribucionInstitucional.nivelDeEstudio.key
         }
         log.debug nivelEstudioEncontrado
       }
-      
-      String clave = filaDominio.cicloEscolar.clave
 
-      def mapa =[ "$clave" : [
-                    "${filaDominio.distribucionInstitucional.nivelDeEstudio.key}" : [
-                      "${filaDominio.distribucionInstitucional.grado}" : [
-                        "${filaDominio.distribucionInstitucional.turno.key}" : [
-                          "${filaDominio.distribucionInstitucional.grupo}" : [
-                            "${filaDominio.dependiente}"
-                          ]
-                        ]
-                      ]
-                    ]
-                  ] 
-                ]
+      String clave = filaDominio.cicloEscolar.clave
+      String nivelDeEstudio = filaDominio.distribucionInstitucional.nivelDeEstudio.key
+
+      def mapa =[:]
+      mapa."$clave" = []
+      mapa."$clave" = [ "$nivelDeEstudio" : []]
+
       estructuraOrganizacional << mapa
     }
 
