7290a32bea65e94c49d249706fef669e8437840f
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-03-27 23:45:24 -0600

    coding #31 verificando datos de prueba... detalles con el GString en el key del mapa dinámico

--- grails-app/services/com/stele/EstructuraInstitucionalService.groovy
+++ grails-app/services/com/stele/EstructuraInstitucionalService.groovy
@@ -6,34 +6,42 @@ class EstructuraInstitucionalService {
     def estructuraOrganizacional = []
     
     listaMapaDominios.each { filaDominio ->
+      def cicloEscolarEncontrado = estructuraOrganizacional.find {
+        it.find { k, v -> k == filaDominio.cicloEscolar.clave }
+      }
 
-    def encontrado = estructuraOrganizacional*.find { k, v ->
-      k == filaDominio.cicloEscolar.clave
-    }
+      if(cicloEscolarEncontrado) {
+        log.debug "cicloEscolarEncontrado : $cicloEscolarEncontrado"
+        log.debug "clave : ${filaDominio.cicloEscolar.clave}"
 
-    log.debug encontrado.class
-    encontrado.each {
-      log.debug it
-    }
+        log.debug cicloEscolarEncontrado."2011-12"
 
-      def mapa = [
-                   "${filaDominio.cicloEscolar.clave}" : [ 
-                     "${filaDominio.distribucionInstitucional.nivelDeEstudio.key}" : [
-                       "${filaDominio.distribucionInstitucional.grado}" : [
-                         "${filaDominio.distribucionInstitucional.turno.key}" : [
-                           "${filaDominio.distribucionInstitucional.grupo}" : [
-                             "${filaDominio.dependiente}"
-                           ]
-                         ]
-                       ]
-                     ]
-                   ] 
-                 ]
 
+        def nivelEstudioEncontrado = cicloEscolarEncontrado."${filaDominio.cicloEscolar.clave}".find { k, v ->
+          log.debug "k : $k"
+          log.debug "filaDominio.distribucionInstitucional.nivelDeEstudio.key : ${filaDominio.distribucionInstitucional.nivelDeEstudio.key}"
+          k == filaDominio.distribucionInstitucional.nivelDeEstudio.key
+        }
+        log.debug nivelEstudioEncontrado
+      }
+      
+      String clave = filaDominio.cicloEscolar.clave
+
+      def mapa =[ "$clave" : [
+                    "${filaDominio.distribucionInstitucional.nivelDeEstudio.key}" : [
+                      "${filaDominio.distribucionInstitucional.grado}" : [
+                        "${filaDominio.distribucionInstitucional.turno.key}" : [
+                          "${filaDominio.distribucionInstitucional.grupo}" : [
+                            "${filaDominio.dependiente}"
+                          ]
+                        ]
+                      ]
+                    ]
+                  ] 
+                ]
       estructuraOrganizacional << mapa
     }
 
     estructuraOrganizacional
   }
-
 }
