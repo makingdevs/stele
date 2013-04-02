8d8f6b63b55405301422488c7006a3004f6b26eb
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-01 23:23:30 -0600

    Coding #32 Refactor DistribucionInstitucionalService

--- grails-app/services/com/stele/DistribucionInstitucionalService.groovy
+++ grails-app/services/com/stele/DistribucionInstitucionalService.groovy
@@ -13,22 +13,31 @@ class DistribucionInstitucionalService {
 
   private NivelDeEstudio validaNivelDeEstudio(String nivel){
     def nivelToUpper = nivel.toUpperCase()
-    def preescolar = /\b${NivelDeEstudio.PREESCOLAR.toString()toUpperCase()substring(0,3)}\w*/
-    def primaria = /\b${NivelDeEstudio.PRIMARIA.toString()toUpperCase()substring(0,3)}\w*/
-    def secundaria = /\b${NivelDeEstudio.SECUNDARIA.toString()toUpperCase()substring(0,3)}\w*/
-    def bachillerato = /\b${NivelDeEstudio.BACHILLERATO.toString()toUpperCase()substring(0,3)}\w*/
-    def educacionSuperior = /\b${NivelDeEstudio.EDUCACION_SUPERIOR.toString()toUpperCase()substring(0,3)}\w*/
+    def preescolar = NivelDeEstudio.PREESCOLAR.toString()toUpperCase()substring(0,3)
+    def primaria = NivelDeEstudio.PRIMARIA.toString()toUpperCase()substring(0,3)
+    def secundaria = NivelDeEstudio.SECUNDARIA.toString()toUpperCase()substring(0,3)
+    def bachillerato = NivelDeEstudio.BACHILLERATO.toString()toUpperCase()substring(0,3)
+    def educacionSuperior = NivelDeEstudio.EDUCACION_SUPERIOR.toString()toUpperCase()substring(0,3)
+    def resultado
 
-    if( ((nivelToUpper =~ preescolar).find()) )
-      return NivelDeEstudio.PREESCOLAR
-    else if( ((nivelToUpper =~ secundaria).find()) )
-      return NivelDeEstudio.SECUNDARIA
-    else if( ((nivelToUpper =~ bachillerato).find()) )
-      return NivelDeEstudio.BACHILLERATO
-    else if( ((nivelToUpper =~ educacionSuperior).find()) )
-      return NivelDeEstudio.EDUCACION_SUPERIOR
-    else
-      return NivelDeEstudio.PRIMARIA
+    switch (nivelToUpper) {
+      case {it.startsWith(preescolar) }:
+        resultado = NivelDeEstudio.PREESCOLAR
+        break
+      case {it.startsWith(secundaria) }:
+        resultado = NivelDeEstudio.SECUNDARIA
+        break
+      case {it.startsWith(bachillerato) }:
+        resultado = NivelDeEstudio.BACHILLERATO
+        break
+      case {it.startsWith(educacionSuperior) }:
+        resultado = NivelDeEstudio.EDUCACION_SUPERIOR
+        break
+      default:
+        resultado = NivelDeEstudio.PRIMARIA
+        break
+    }
+    resultado
   }
 
   private Turno validaTurno(String turno){
