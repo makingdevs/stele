f81db82497914e8a7015aa9eb1c29f9ed46bc173
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-04-06 10:54:08 -0600

    coding #37 primer caso de prueba

---
+++ test/unit/com/stele/ReporteMigracionServiceSpec.groovy
@@ -0,0 +1,25 @@
+package com.stele
+
+import grails.test.mixin.*
+import org.junit.*
+import spock.lang.Specification
+
+@TestFor(ReporteMigracionService)
+class ReporteMigracionServiceSpec extends Specification {
+
+  def "Obtener conteo de dependientes" () {
+
+    when : "Llamamos al contador de datos"
+      def totalDependientes = service.conteoDeDatosPorDependientes(estructuraInstitucional)
+
+    then : "Los resultados son"
+      assert totalDependientes == totalDependientesEsperados
+
+    where :
+      estructuraInstitucional                                   || totalDependientesEsperados
+      ['20':['NIVEL':['1':['TURNO':['A' : [1,2,3,4,5,6,10]]]]]] || 7
+      
+
+  }
+
+}
