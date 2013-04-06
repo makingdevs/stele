991be6c14e8f7cbb74fe75e6b60674decc82c133
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-06 13:02:24 -0600

    coding #38 definiendo nuevo nombre para el nomnbre de servicio y metodo para procesamiento masivo

--- grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
+++ grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
@@ -5,7 +5,7 @@ class ProcesamientoMasivoController {
   def index() {
     def listaDeMapaDeDominios = flash.listaDeMapaDeDominios
     listaDeMapaDeDominios.each { l ->
-      registroService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l)     
+      procesamientoMasivoService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l)     
     }
   }
 }
