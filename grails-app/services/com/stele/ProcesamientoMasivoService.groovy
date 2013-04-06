7450387a53eb3ee015194d4952af402e0eacc7a9
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-06 12:10:28 -0600

    coding #34 agregando el resgitro masivo service

---
+++ grails-app/services/com/stele/ProcesamientoMasivoService.groovy
@@ -0,0 +1,8 @@
+package com.stele
+
+class ProcesamientoMasivoService {
+
+    def serviceMethod() {
+
+    }
+}
---
+++ test/unit/com/stele/ProcesamientoMasivoServiceSpec.groovy
@@ -0,0 +1,12 @@
+package com.stele
+
+import grails.test.mixin.*
+import org.junit.*
+import spock.lang.Specification
+
+@TestFor(ProcesamientoMasivoService)
+class ProcesamientoMasivoServiceSpec extends Specification {
+
+    def "Procesa una fila de datos unica"(){}
+
+}
