cc56452bd60f22f18e9482bf5ff82f2e8c80bbec
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-04-06 11:39:16 -0600

    coding #37 haciendo pasar la prueba

--- grails-app/services/com/stele/ReporteMigracionService.groovy
+++ grails-app/services/com/stele/ReporteMigracionService.groovy
@@ -2,6 +2,13 @@ package com.stele
 
 class ReporteMigracionService {
 
+  def conteoDeDatosPorDependientes(estructuraInstitucional) {
+    if( estructuraInstitucional instanceof List ) {
+      return estructuraInstitucional.size()
+    }
 
+    def llave = estructuraInstitucional.collect { k, v -> k }
+    conteoDeDatosPorDependientes( estructuraInstitucional.(llave[0]) )
+  }
 
 }
