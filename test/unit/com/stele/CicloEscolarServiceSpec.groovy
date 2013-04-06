d49c91c47c7e4c8c658f5dceefdc0dc1e76df544
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-06 12:43:54 -0600

    coding #40 agregando caso de pruebda para el registro de ciclo escolar

--- test/unit/com/stele/CicloEscolarServiceSpec.groovy
+++ test/unit/com/stele/CicloEscolarServiceSpec.groovy
@@ -29,4 +29,16 @@ class CicloEscolarServiceSpec extends Specification{
         ]
   }
 
+  def "Registro de un ciclo escolar verificando que no exista"(){
+    given: "Un ciclo escolar con datos"
+    when: "Guardamos el ciclo escolar con el servicio"
+    then: "El id debe ser mayor que 0"
+  }
+
+  def "No agregar un ciclo escolar ya existente"(){
+    given: "Un ciclo escolar con datos"
+    when: "Intentamos guardar ciclo escolar"
+    then: "El id debe ser 1"
+  }
+
 }
