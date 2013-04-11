1473fa83ee5e14bf5b08c8da0fa644238fe230e1
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-10 22:56:38 -0500

    coding #40 re-indent ciclo escolar service y usuario service

--- grails-app/services/com/stele/CicloEscolarService.groovy
+++ grails-app/services/com/stele/CicloEscolarService.groovy
@@ -2,14 +2,14 @@ package com.stele
 
 class CicloEscolarService {
 
-    def obtenerCicloEscolarDesdeCommand(FilaExcelCommand filaExcelCommand) {
-      def cicloEscolar = new CicloEscolar()
-      cicloEscolar.clave = filaExcelCommand.cicloEscolar
-      cicloEscolar
-    }
+  def obtenerCicloEscolarDesdeCommand(FilaExcelCommand filaExcelCommand) {
+    def cicloEscolar = new CicloEscolar()
+    cicloEscolar.clave = filaExcelCommand.cicloEscolar
+    cicloEscolar
+  }
 
-    def registrar(CicloEscolar cicloEscolar){
-      def existeCicloEscolar = CicloEscolar.findByClave(cicloEscolar.clave)
-      existeCicloEscolar ?: cicloEscolar.save()
-    }
+  def registrar(CicloEscolar cicloEscolar){
+    def existeCicloEscolar = CicloEscolar.findByClave(cicloEscolar.clave)
+    existeCicloEscolar ?: cicloEscolar.save()
+  }
 }
