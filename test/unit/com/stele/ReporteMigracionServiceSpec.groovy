8a5d1a0c80720908fe3c8066a5e3ad2ad5ca9f75
fjuarezmurillo <felipe.juarez@synergyj.com>
2013-04-10 20:50:02 -0500

    coding #37 agregando casos de prueba para el conteo por nivel y grado

--- test/unit/com/stele/ReporteMigracionServiceSpec.groovy
+++ test/unit/com/stele/ReporteMigracionServiceSpec.groovy
@@ -57,4 +57,22 @@ class ReporteMigracionServiceSpec extends Specification {
        '22':['NIVEL':['1':['TURNO1':['A' : [  6,7]]]]]]             || ['21.NIVEL': 5, '21.NIVEA':4, '22.NIVEL':2]
   }
 
+  def "Obtener conteo de dependientes por nivel y grado" () {
+    when : "Llamamos al contador de datos"
+      def totalDependientes = service.conteoDeDependientesPorNivelYGrado(estructuraInstitucional)
+
+    then : "Los resultados son"
+      assert totalDependientes == totalDependientesEsperados
+
+    where :
+      estructuraInstitucional                                       || totalDependientesEsperados
+      ['21':['NIVEL':['1':['TURNO1':['A' : [1,2,3]]]]]]             || ['21.NIVEL.1': 3]
+
+      ['21':['NIVEL':['1':['TURNO1':['A' : [1,2,3]]]]],
+       '22':['NIVEL':['1':['TURNO1':['A' : [  6,7]]]]]]             || ['21.NIVEL.1': 3, '22.NIVEL.1':2]
+
+      ['21':['NIVEL':['1':['TURNO1':['A' : [1,2,3], 'B' : [1,2]]]
+                      '2':['TURNO1':['A' : [1,2,3,5]]]]],
+       '22':['NIVEL':['1':['TURNO1':['A' : [  6,7]]]]]]             || ['21.NIVEL.1': 5, '21.NIVEL.2':4, '22.NIVEL.1':2]
+  }
 }
