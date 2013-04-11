ea7f5a8bfd7c5b286bfe804f814e39b4e999034b
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-10 22:56:03 -0500

    coding #40 agregando prueba para ciclo escolar ya existente

--- test/unit/com/stele/CicloEscolarServiceSpec.groovy
+++ test/unit/com/stele/CicloEscolarServiceSpec.groovy
@@ -12,41 +12,57 @@ import spock.lang.Specification
 class CicloEscolarServiceSpec extends Specification{
 
   def "Obtener un ciclo escolar apartir de un command leeido desde un excel"(){
-
     given:
-        def filaExcelCommand = new FilaExcelCommand(datosBasicos) 
-      when:
-        def cicloEscolar = service.obtenerCicloEscolarDesdeCommand(filaExcelCommand)
-      then:
-        assert cicloEscolar.clave == cicloEscolarEsperado
-      where:
-        datosBasicos << [
-          [cicloEscolar:"12345"],
-          [cicloEscolar:"ADD12345"]
-        ]
-        cicloEscolarEsperado << [
-          "12345",
-          "ADD12345"
-        ]
+    def filaExcelCommand = new FilaExcelCommand(datosBasicos) 
+    when:
+    def cicloEscolar = service.obtenerCicloEscolarDesdeCommand(filaExcelCommand)
+    then:
+    assert cicloEscolar.clave == cicloEscolarEsperado
+    where:
+    datosBasicos << [
+    [cicloEscolar:"12345"],
+    [cicloEscolar:"ADD12345"]
+    ]
+    cicloEscolarEsperado << [
+    "12345",
+    "ADD12345"
+    ]
   }
 
   def "Registro de un ciclo escolar verificando que no exista"(){
     given: "Un ciclo escolar con datos"
-      def cicloEscolar = new CicloEscolar()
-      cicloEscolar.clave = "1234567"
-      cicloEscolar.descripcion = "Prueba ciclo escolar"
-      cicloEscolar.fechaInicio = new Date()
-      cicloEscolar.fechaFin = new Date()
+    def cicloEscolar = new CicloEscolar()
+    cicloEscolar.clave = "1234567"
+    cicloEscolar.descripcion = "Prueba ciclo escolar"
+    cicloEscolar.fechaInicio = new Date()
+    cicloEscolar.fechaFin = new Date()
     when: "Guardamos el ciclo escolar con el servicio"
-      cicloEscolar = service.registrar(cicloEscolar)
+    cicloEscolar = service.registrar(cicloEscolar)
     then: "El id debe ser mayor que 0"
-      assert cicloEscolar.id > 0
+    assert cicloEscolar.id > 0
   }
 
   def "No agregar un ciclo escolar ya existente"(){
-    given: "Un ciclo escolar con datos"
+    given: "Un ciclo escolar ya existente y "
+    def cicloEscolarExistente = new CicloEscolar()
+    cicloEscolarExistente.id = 25
+    cicloEscolarExistente.clave = "1234567"
+    cicloEscolarExistente.descripcion = "Prueba ciclo escolar"
+    cicloEscolarExistente.fechaInicio = new Date()
+    cicloEscolarExistente.fechaFin = new Date()
+    service.registrar(cicloEscolarExistente)
+    def contador = CicloEscolar.count()
+    and: "Un ciclo escolar con datos"
+    def cicloEscolar = new CicloEscolar()
+    cicloEscolar.clave = "1234567"
+    cicloEscolar.descripcion = "Prueba ciclo escolar"
+    cicloEscolar.fechaInicio = new Date()
+    cicloEscolar.fechaFin = new Date()
     when: "Intentamos guardar ciclo escolar"
-    then: "El id debe ser 1"
+    cicloEscolar = service.registrar(cicloEscolar)
+    then: "El id debe ser igual a 25"
+    assert cicloEscolar.id == 25
+    assert contador == CicloEscolar.count()
   }
 
 }
