b2ce0b6cb9193ee5dd28b7e0119658b4849c5a42
rodrigomg <rockdrigo.mtz@gmail.com>
2013-04-15 22:19:40 -0500

    coding #41 validando casos para implementación de compareTo dentro de distribucion institucional

--- test/unit/com/stele/DistribucionInstitucionalSpec.groovy
+++ test/unit/com/stele/DistribucionInstitucionalSpec.groovy
@@ -7,25 +7,35 @@ import org.junit.*
 import spock.lang.Specification
 import spock.lang.Unroll
 
+
 class DistribucionInstitucionalSpec extends Specification{
 
+      @Unroll("#datosDistribucionInstitucional vs #datosDistribucionInstitucionalII")
      def "Validando la implementacion de compareTo en distribucion institucional"(){
       given: "Una distribucion institucional"
-        DistribucionInstitucional distribucionInstitucional = new DistribucionInstitucional() 
-        distribucionInstitucional.nivelDeEstudio = NivelDeEstudio.PRIMARIA
-        distribucionInstitucional.grado = 1
-        distribucionInstitucional.grupo = "B"
-        distribucionInstitucional.turno = Turno.MATUTINO
+        DistribucionInstitucional distribucionInstitucional = new DistribucionInstitucional(datosDistribucionInstitucional) 
       and:"Otra distribucion institucional diferente"
-        DistribucionInstitucional distribucionInstitucionalII = new DistribucionInstitucional() 
-        distribucionInstitucionalII.nivelDeEstudio = NivelDeEstudio.PRIMARIA
-        distribucionInstitucionalII.grado = 1
-        distribucionInstitucionalII.grupo = "B"
-        distribucionInstitucionalII.turno = Turno.MATUTINO
-      when: "Se hace la comparacion"
+        DistribucionInstitucional distribucionInstitucionalII = new DistribucionInstitucional(datosDistribucionInstitucionalII) 
+      when:"Se hace la comparacion"
         def valorComparacion = distribucionInstitucional <=> distribucionInstitucionalII
       then:"Se valida el resultado"
-        assert true 
+        assert valorComparacion == valorEsperado 
         println valorComparacion
+       where:
+        datosDistribucionInstitucional << [
+          [nivelDeEstudio:NivelDeEstudio.PRIMARIA,grado:"2",grupo:"C",turno:Turno.MATUTINO],
+          [nivelDeEstudio:NivelDeEstudio.SECUNDARIA,grado:"2",grupo:"C",turno:Turno.NOCTURNO],
+          [nivelDeEstudio:NivelDeEstudio.BACHILLERATO,grado:"1",grupo:"C",turno:Turno.MATUTINO]
+        ]
+        datosDistribucionInstitucionalII << [
+          [nivelDeEstudio:NivelDeEstudio.PRIMARIA,grado:"2",grupo:"C",turno:Turno.MATUTINO],
+          [nivelDeEstudio:NivelDeEstudio.PRIMARIA,grado:"2",grupo:"C",turno:Turno.VESPERTINO],
+          [nivelDeEstudio:NivelDeEstudio.EDUCACION_SUPERIOR,grado:"2",grupo:"B+",turno:Turno.NOCTURNO]
+        ]
+        valorEsperado << [
+          0,
+          1,
+          -1
+        ]
     }
 }
