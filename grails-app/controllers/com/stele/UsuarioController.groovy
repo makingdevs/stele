169156553e6ff9781ac47701baa8cc551917a054
Felipe Juarez <felipe.juarez@synergyj.com>
2013-04-29 13:41:54 -0500

    coding #35 aumentando los datos del dependiente

--- grails-app/controllers/com/stele/UsuarioController.groovy
+++ grails-app/controllers/com/stele/UsuarioController.groovy
@@ -6,12 +6,25 @@ class UsuarioController {
 
   static scaffold = Usuario
 
+  def springSecurityService
+
   def edicion() {
   }
 
   def obtenerPerfilUsuario() {
+    def usuarioActual = springSecurityService.currentUser
+    log.debug "username : ${usuarioActual}"
+    log.debug "dependientes : ${usuarioActual.dependientes}"
+
     render(contentType:"text/json") {
-      [nombreTutor : "Nelson", apellidosTutor : "Muntz", urlImagenTutor : "http://www.blogdelossimpson.com.ar/wp-content/uploads/2008/08/nelson1.gif"]
+      [
+        nombreTutor : usuarioActual.perfil.nombre,
+        apellidosTutor : "${usuarioActual.perfil.apellidoPaterno} ${usuarioActual.perfil.apellidoMaterno ?: ''}",
+        urlImagenTutor : "http://www.blogdelossimpson.com.ar/wp-content/uploads/2008/08/nelson1.gif",
+        dependientes : [
+          usuarioActual.dependientes
+        ]
+      ]
     }
   }
 
