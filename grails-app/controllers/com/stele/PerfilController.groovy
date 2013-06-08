d61de023a73732b0ce9c6b2a212c662d35e60653
neodevelop <neodevelop@gmail.com>
2013-06-08 11:59:03 -0500

    coding #53 visualizando los errores y llamando al servicio de actualizar passwords

--- grails-app/controllers/com/stele/PerfilController.groovy
+++ grails-app/controllers/com/stele/PerfilController.groovy
@@ -3,15 +3,24 @@ package com.stele
 class PerfilController {
 
   def springSecurityService
+  def perfilService
 
   def index() {
     def usuarioActual = springSecurityService.currentUser
     [usuarioActual : usuarioActual]
   }
 
-  def actualizarPassword() {
-    log.debug "params : ${params}"
+  def actualizarPassword(UpdatePasswordCommand upc) {
+    log.debug "actualizando passwords"
+
+    if(upc.hasErrors()) {
+      render upc.errors
+      return 
+    }
+
+    perfilService.actualizarPasswordForUser(upc, springSecurityService.currentUser)
     redirect controller:'perfil'
   }
 
 }
+
