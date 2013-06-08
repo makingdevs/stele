cc70069e158e8494d87f48edd665f9ba44b030e6
neodevelop <neodevelop@gmail.com>
2013-06-08 12:55:51 -0500

    arreglando pruebas

--- grails-app/domain/com/stele/Perfil.groovy
+++ grails-app/domain/com/stele/Perfil.groovy
@@ -9,7 +9,7 @@ class Perfil {
   String apellidoMaterno
   Date fechaDeNacimiento
   Sexo sexo = Sexo.QUIMERA
-  String avatar
+  String avatar = "http://images3.wikia.nocookie.net/__cb20121205194059/simpsons/images/e/e9/Nelson_Ha-Ha.jpg"
 
   static hasMany = [direccion: Direccion, telefonos: Telefono]
 
@@ -21,6 +21,7 @@ class Perfil {
     apellidoPaterno size:1..50,blank:false
     apellidoMaterno size:1..50,blank:true,nullable:true
     fechaDeNacimiento nullable:true
+    avatar url:true
   }
 
   String nombreCompleto() {
