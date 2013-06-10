a300fcad1b8ecaf4e5babaff927f4d27961ea351
Rockdrigo Martínez <rockdrigo.mtz@gmail.com>
2013-06-10 18:26:39 -0500

    feature #44 eliminando error al persistir dependiente por falta de parametros

--- grails-app/services/com/stele/ProcesamientoMasivoService.groovy
+++ grails-app/services/com/stele/ProcesamientoMasivoService.groovy
@@ -9,7 +9,7 @@ class ProcesamientoMasivoService {
 
   def procesaMapaConDatosDeFilaDeExcelParaPersistir(def filaDeExcelParaPersistir) {
     def usuario = usuarioService.registrar(filaDeExcelParaPersistir.usuario)
-    def dependiente = dependienteService.registrar(filaDeExcelParaPersistir.dependiente)
+    def dependiente = dependienteService.registrar(filaDeExcelParaPersistir.dependiente, usuario.id)
     def cicloEscolar = cicloEscolarService.registrar(filaDeExcelParaPersistir.cicloEscolar)
     def distribucionInstitucional = distribucionInstitucionalService.registrar(filaDeExcelParaPersistir.distribucionInstitucional)
     [usuario: usuario, dependiente: dependiente, cicloEscolar: cicloEscolar, distribucionInstitucional: distribucionInstitucional]
