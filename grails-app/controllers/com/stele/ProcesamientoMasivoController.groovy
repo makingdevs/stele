08d3e4ec7e6100d3340571016cc586ef7abe95bc
rodrigomg <rockdrigo.mtz@gmail.com>
2013-06-13 21:47:48 -0500

    coding #56 Primer acercamiento a asignacion de camada a dependientes

--- grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
+++ grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
@@ -6,8 +6,12 @@ class ProcesamientoMasivoController {
 
   def index() {
     def listaDeMapaDeDominios = flash.listaDeMapaDeDominios
+    //TODO: Camada = nombre de institucion, reemplazar espacion por _ + fecha separada por _ (DD_MM_YY_HH_MM)
+    Institucion institucion = Institucion.get(params.long("institucionId"))
+    listaDeMapaDeDominios*.dependiente*.camada = institucion.nombre
     listaDeMapaDeDominios.each { l ->
-      procesamientoMasivoService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l,params.long("institucionId") )
+      //procesamientoMasivoService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l,params.long("institucionId") )
+      log.debug "Camada: " + l.dependiente.camada 
     }
   }
 }
