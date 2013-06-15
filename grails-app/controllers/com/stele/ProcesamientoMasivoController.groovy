1a136ca132215c3f6124d7e9bd7447468dc23adf
rodrigomg <rockdrigo.mtz@gmail.com>
2013-06-15 10:35:57 -0500

    coding #56 agregando la camada a dependiente antes de persistir la información, formada por nombre_de_institucion+fecha_dia_mes_año_hora_minuto

--- grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
+++ grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
@@ -6,12 +6,9 @@ class ProcesamientoMasivoController {
 
   def index() {
     def listaDeMapaDeDominios = flash.listaDeMapaDeDominios
-    //TODO: Camada = nombre de institucion, reemplazar espacion por _ + fecha separada por _ (DD_MM_YY_HH_MM)
-    Institucion institucion = Institucion.get(params.long("institucionId"))
-    listaDeMapaDeDominios*.dependiente*.camada = institucion.nombre
+    listaDeMapaDeDominios*.dependiente*.camada = Institucion.get(params?.long("institucionId")).nombre.replaceAll(" ","_") + new Date().format("dd_MM_yy_HH_mm")
     listaDeMapaDeDominios.each { l ->
-      //procesamientoMasivoService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l,params.long("institucionId") )
-      log.debug "Camada: " + l.dependiente.camada 
+      procesamientoMasivoService.procesaMapaConDatosDeFilaDeExcelParaPersistir(l,params.long("institucionId") )
     }
   }
 }
