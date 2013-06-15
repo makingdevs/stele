e3645069b77815705a04ab00fdb677495ae232ee
neodveelop <neodevelop@gmail.com>
2013-06-15 12:24:32 -0500

    coding #51 agregando acciones para la consulta de camada

--- grails-app/controllers/com/stele/CamadaController.groovy
+++ grails-app/controllers/com/stele/CamadaController.groovy
@@ -2,5 +2,14 @@ package com.stele
 
 class CamadaController {
 
-    def index() { }
+  def show(){
+    String camada = params.camada
+    if(!camada || camada == 'NaC'){
+      flash.message = "No hemos identificado el grupo de alumnos!!!"
+      redirect uri:"/"
+      return
+    }
+    def dependientes = Dependiente.findByCamada(camada)
+    [dependientes:dependientes]
+  }
 }
---
+++ grails-app/views/camada/show.gsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="layout" content="twitterBootstrap"/>
+    <title>Preview</title>
+  </head>
+  <body>
+    <div class="page-header">
+      <h1>Listado de alumnos</h1>
+    </div>
+  </body>
+</html>
