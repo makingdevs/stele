f5b7d7ba0606fc8788cfa48eea969d85eb794320
Rockdrigo Martínez <rockdrigo.mtz@gmail.com>
2013-07-02 17:14:09 -0500

    coding #76 incluyendo en procesamiento masivo el registro del historial academico

--- grails-app/services/com/stele/ProcesamientoMasivoService.groovy
+++ grails-app/services/com/stele/ProcesamientoMasivoService.groovy
@@ -6,12 +6,14 @@ class ProcesamientoMasivoService {
   def dependienteService
   def cicloEscolarService
   def distribucionInstitucionalService
+  def historialAcademicoService
 
   def procesaMapaConDatosDeFilaDeExcelParaPersistir(def filaDeExcelParaPersistir,Long institucionId) {
     def usuario = usuarioService.registrar(filaDeExcelParaPersistir.usuario)
     def dependiente = dependienteService.registrar(filaDeExcelParaPersistir.dependiente, usuario.id)
     def cicloEscolar = cicloEscolarService.registrar(filaDeExcelParaPersistir.cicloEscolar)
     def distribucionInstitucional = distribucionInstitucionalService.registrar(filaDeExcelParaPersistir.distribucionInstitucional, institucionId)
+    def historialAcademico = historialAcademicoService.registrar(historialAcademicoService.preparaHistoricoAcademicoARegistrar(dependiente,distribucionInstitucional))
     [usuario: usuario, dependiente: dependiente, cicloEscolar: cicloEscolar, distribucionInstitucional: distribucionInstitucional]
   }
 }
