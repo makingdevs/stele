6c839ea62cd5728a16d3bbd724538d0ba66ed036
Rockdrigo Martínez <rockdrigo.mtz@gmail.com>
2013-07-05 12:10:15 -0500

    coding #79 agregando logica a servicio y en la prueba para consultar pagos por usuario conectado

--- grails-app/services/com/stele/PagoService.groovy
+++ grails-app/services/com/stele/PagoService.groovy
@@ -1,8 +1,19 @@
 package com.stele
 
+import com.stele.seguridad.Usuario
+
 class PagoService {
 
-    def serviceMethod() {
-
+    def obtenerPagosDeUsuario(Usuario usuario) {
+      def criteriaDependiente = Dependiente.createCriteria()
+      def pagosDeUsuario = [] as Set 
+      List<Dependiente> dependientesDeUsuario = criteriaDependiente.list {
+          eq("usuario", usuario)
+      }
+      dependientesDeUsuario.each{ dependiente ->
+        Dependiente dependienteExistente = dependiente.get(dependiente.id)
+        pagosDeUsuario.addAll(dependienteExistente.pagos)
+      }
+      pagosDeUsuario
     }
 }
