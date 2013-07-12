8807e5d35f84c50372b4390a514d771dd0c41c2d
rodrigomg <rockdrigo.mtz@gmail.com>
2013-07-11 22:17:50 -0500

    coding #72 creando servicio y prueba para comprobante

---
+++ grails-app/services/com/stele/ComprobanteService.groovy
@@ -0,0 +1,21 @@
+package com.stele
+
+import org.grails.s3.S3Asset
+
+class ComprobanteService {
+
+  def s3AssetService
+
+  def agregarComprobanteAPago(Long pagoId, file) {
+    S3Asset receipt = new S3Asset()
+    Pago pago = Pago.get(pagoId)
+    def tmp = s3AssetService.getNewTmpLocalFile(file.contentType)
+    file.transferTo(tmp)
+    receipt.newFile(tmp)
+    receipt.mimeType = file.contentType
+    s3AssetService.put(receipt)
+    pago.comprobanteDePago = receipt
+    pago.estatusDePago = EstatusDePago.PROCESO
+    pago.save()
+  }
+}
