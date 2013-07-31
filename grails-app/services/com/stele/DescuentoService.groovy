c650e62d908a3a64dd7c2c24eb6036075a7c6c3c
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-07-31 13:31:03 -0500

    coding #86 se ajusta el metodo de busqueda para localizar coincidencias en los nombres de los descuentos

--- grails-app/services/com/stele/DescuentoService.groovy
+++ grails-app/services/com/stele/DescuentoService.groovy
@@ -6,9 +6,11 @@ import com.stele.Institucion
 
 class DescuentoService {
 
-  def buscarDescuentosDeUnaInstitucion(Usuario usuario) {
+  def buscarDescuentosDeUnaInstitucion(Usuario usuario, def query) {
+
     def institucion = usuario.instituciones
     Descuento .withCriteria {
+      like('nombreDeDescuento', "%${query}%" )
       'in'('institucion', institucion) 
     }
   }
