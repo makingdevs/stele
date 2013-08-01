ad06317a182b51bba16ffd90f43d29751d679586
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-01 16:35:19 -0500

    coding #86 se crea el servico que persiste los descuentos en la db

--- grails-app/controllers/com/stele/DescuentoController.groovy
+++ grails-app/controllers/com/stele/DescuentoController.groovy
@@ -12,11 +12,34 @@ class DescuentoController {
 
   def index() { }
 
+  def nuevo() {
+    log.debug "descuentos : $params"
+    Descuento descuento = findOrSaveDescuentoWithParams(params)
+
+    def descuentosIds = [descuento.id]
+    if(params.descuentosIds) {
+      def listaDescuentos = params.descuentosIds?.replace('[','')?.replace(']','')?.split(',') ?: []
+      descuentosIds += listaDescuentos.collect{ s -> Long.valueOf(s) }
+    }
+    log.debug(descuentosIds)
+    render template:"/descuento/list", model:[descuentos:Descuento.getAll(descuentosIds), descuentosIds:descuentosIds]
+  }
+
   def obtenerDescuentosInstitucion() {
     def descuentosList = descuentoService.buscarDescuentosDeUnaInstitucion(springSecurityService.currentUser,params.query)
     JSON.use('stele') {
       render descuentosList as JSON
     }
+  }
+
+  private Descuento findOrSaveDescuentoWithParams(params) {
+    Descuento descuento = Descuento.findByNombreDeDescuento(params.nombreDeDescuento)
+    if(!descuento) {
+      descuento = new Descuento(params)
+      descuento.institucion = springSecurityService.currentUser.instituciones?.first()  
+      descuento.save(flush:true)
+    }
+    descuento
   }
 
 }
