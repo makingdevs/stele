0cc82454a08dee30ba4ba80f30ce2591df135d41
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-02 14:22:46 -0500

    coding #86 FixTest1 : se ajusta los parametros de recepcion del servicio

--- test/unit/com/stele/ConceptoServiceSpec.groovy
+++ test/unit/com/stele/ConceptoServiceSpec.groovy
@@ -8,7 +8,7 @@ import com.stele.seguridad.Usuario
 @Mock([Institucion,Usuario,DistribucionInstitucional,Concepto,Perfil])
 class ConceptoServiceSpec extends Specification {
 
-    def "Obtener un listado de los descuentos ligados a una Institucion"() {
+    def "Obtener un listado de los conceptos ligados a una Institucion"() {
   given: "Se crearan 2 conceptos asociados a una Institucion deacuerdo al Usuario"
     def institucion = new Institucion()
     institucion.nombre = "Escuela primaria de Springfild"
@@ -41,11 +41,12 @@ class ConceptoServiceSpec extends Specification {
     concepto1.institucion = institucion
     concepto1.save(flush:true)
     concepto2.id = 2
-    concepto2.concepto = "Primera parcialidad"
+    concepto2.concepto = "Primera colegiatura"
     concepto2.institucion = institucion
     concepto2.save(flush:true)
   when: "Se realiza la llamada al metodo buscarConceptosDeUnaInstitucion"
-    def conceptoInstitucion = service.buscarConceptosDeUnaInstitucion(usuario)
+    def query = "giat"
+    def conceptoInstitucion = service.buscarConceptosDeUnaInstitucion(usuario, query)
   then: "la cantidad de conceptos debe de ser igual a 2"
     assert conceptoInstitucion.size() == 2
 
