e34cb5ac7b825ae9ade7e86ede1118d5b1d4e8fd
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-02 16:34:57 -0500

    coding #86 se ajusta el servicio para asociar los pagos a los descuentos

--- grails-app/services/com/stele/GeneracionDePagoService.groovy
+++ grails-app/services/com/stele/GeneracionDePagoService.groovy
@@ -7,12 +7,13 @@ class GeneracionDePagoService {
 
   def conceptoService
 
-  def paraCamadaPagoCommand(CamadaPagoCommand camadaPagoCommand) {
+  def paraCamadaPagoCommand(CamadaPagoCommand camadaPagoCommand, Usuario usuario, def descuentos) {
 
     def dependientes = Dependiente.findAllByCamada(camadaPagoCommand.camada)
+    conceptoService.guardarConceptoDePagoGenerado(usuario, camadaPagoCommand.properties.conceptoDePago)
     List<Pago> pagos = []
     dependientes.each { dependiente ->
-      def pago = generarPagoParaDependienteConCommand(dependiente, camadaPagoCommand).save()
+      def pago = generarPagoParaDependienteConCommand(dependiente, camadaPagoCommand, descuentos).save()
       dependiente.addToPagos(pago)
       dependiente.save()
       pagos << pago
@@ -20,12 +21,22 @@ class GeneracionDePagoService {
     pagos
   }
 
-  private def generarPagoParaDependienteConCommand(Dependiente dependiente, CamadaPagoCommand camadaPagoCommand) {
+  private def generarPagoParaDependienteConCommand(Dependiente dependiente, CamadaPagoCommand camadaPagoCommand, def descuentos) {
+    def list = []
+    descuentos.each { id ->
+      list.add(id.toLong())
+    }
+    def descuentoList = Descuento.withCriteria {
+      'in'('id', list)
+    }
     HistorialAcademico historialAcademico = dependiente.historialAcademico.max {
       it.dateCreated
     }
     Pago pago = new Pago(camadaPagoCommand.properties)
     pago.historialAcademico = historialAcademico
+    descuentoList.each { descuento ->
+      pago.addToDescuentos(descuento)
+    }
     pago
   }
 
