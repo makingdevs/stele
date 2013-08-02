2f2f5f29b024e714e915b23699d9acdfc2c366a1
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-02 12:48:52 -0500

    coding #86 se agrega los comandos faltantes para funcionamiento optimo de la prueba

--- test/unit/com/stele/ConceptoServiceSpec.groovy
+++ test/unit/com/stele/ConceptoServiceSpec.groovy
@@ -8,7 +8,7 @@ import com.stele.seguridad.Usuario
 @Mock([Institucion,Usuario,DistribucionInstitucional,Concepto,Perfil])
 class ConceptoServiceSpec extends Specification {
 
-    def "Obtener un listado de los descuentos ligados a una Institucion"() {
+    def "Obtener un listado de los conceptos ligados a una Institucion"() {
   given: "Se crearan 2 conceptos asociados a una Institucion deacuerdo al Usuario"
     def institucion = new Institucion()
     institucion.nombre = "Escuela primaria de Springfild"
@@ -25,27 +25,28 @@ class ConceptoServiceSpec extends Specification {
     def perfilUsuario = new Perfil()
     def concepto1 = new Concepto()
     def concepto2 = new Concepto()
-    usuario.id = 1033
+    usuario.id = 103123
     usuario.username = "sergio@makingdevs.com"
     usuario.password = UUID.randomUUID().toString().replaceAll('-', '').substring(0,10)
     usuario.enabled = true
     perfilUsuario.nombre = "Jose"
     perfilUsuario.apellidoPaterno = "Hernandez"
     perfilUsuario.apellidoMaterno = "Solis"
-    perfilUsuario.save(flush:true)
+    perfilUsuario.save()
     usuario.perfil = perfilUsuario
     usuario.addToInstituciones(institucion)
-    usuario.save(flush:true)
+    usuario.save()
     concepto1.id = 1
     concepto1.concepto = "Colegiatura"
     concepto1.institucion = institucion
-    concepto1.save(flush:true)
+    concepto1.save()
     concepto2.id = 2
-    concepto2.concepto = "Primera parcialidad"
+    concepto2.concepto = "Primera colegiatura"
     concepto2.institucion = institucion
-    concepto2.save(flush:true)
+    concepto2.save()
   when: "Se realiza la llamada al metodo buscarConceptosDeUnaInstitucion"
-    def conceptoInstitucion = service.buscarConceptosDeUnaInstitucion(usuario)
+    def query = "gia"
+    def conceptoInstitucion = service.buscarConceptosDeUnaInstitucion(usuario, query)
   then: "la cantidad de conceptos debe de ser igual a 2"
     assert conceptoInstitucion.size() == 2
 
