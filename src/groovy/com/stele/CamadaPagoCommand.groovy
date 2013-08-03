0a5c02b2e711ee9e5518ed5e244317f8bdf36a84
says33 <says.rodriguez@gmail.com>
2013-08-02 21:31:38 -0500

    ·coding #86 se agrega lña variable descuentos

--- src/groovy/com/stele/CamadaPagoCommand.groovy
+++ src/groovy/com/stele/CamadaPagoCommand.groovy
@@ -9,12 +9,14 @@ class CamadaPagoCommand {
   String conceptoDePago
   BigDecimal cantidadDePago
   Date fechaDeVencimiento
+  Object descuentos
 
   static constraints = {
     camada size:1..100, blank:false, nullable:false
     conceptoDePago size:1..150, blank:false, nullable:false
     cantidadDePago min:1.0, nullable:false
     fechaDeVencimiento nullable: false
+    descuentos nullable:true
   }
 
 }
