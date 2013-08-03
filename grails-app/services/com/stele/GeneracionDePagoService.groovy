95dc81b7f1c845a4ffbc04a6695d467d59626ba8
says33 <says.rodriguez@gmail.com>
2013-08-02 21:44:56 -0500

    coding #86 se agregan los descuentos a los pagos si existe un registro de descuento creado en la instancia de la generacion de pagos

--- grails-app/services/com/stele/GeneracionDePagoService.groovy
+++ grails-app/services/com/stele/GeneracionDePagoService.groovy
@@ -1,12 +1,13 @@
 package com.stele
 
+import com.stele.Descuento
+
 class GeneracionDePagoService {
 
   def conceptoService
   def springSecurityService
 
   def paraCamadaPagoCommand(CamadaPagoCommand camadaPagoCommand) {
-
     conceptoService.guardarConceptoDePagoGenerado(springSecurityService.currentUser,camadaPagoCommand.conceptoDePago)
 
     def dependientes = Dependiente.findAllByCamada(camadaPagoCommand.camada)
@@ -22,11 +23,22 @@ class GeneracionDePagoService {
   }
 
   private def generarPagoParaDependienteConCommand(Dependiente dependiente, CamadaPagoCommand camadaPagoCommand) {
+    def listaIdDescuentos = []
+    camadaPagoCommand.descuentos.each { descuetoId ->
+      if (descuetoId)
+        listaIdDescuentos.add(descuetoId.toLong())
+    }
+    def listaDeDescuentosParaAplicar = Descuento.withCriteria {
+      'in'('id', listaIdDescuentos)
+    }
     HistorialAcademico historialAcademico = dependiente.historialAcademico.max {
       it.dateCreated
     }
     Pago pago = new Pago(camadaPagoCommand.properties)
     pago.historialAcademico = historialAcademico
+    listaDeDescuentosParaAplicar.each { descuento ->
+      pago.addToDescuentos(descuento)
+    }
     pago
   }
 
