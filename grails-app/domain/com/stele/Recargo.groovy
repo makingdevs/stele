ec569d444eb0e50be2f63410a98683de1ff77ebb
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-05 14:52:15 -0500

    coding #108 se modifican los constraigns de la clase recargo

--- grails-app/domain/com/stele/Recargo.groovy
+++ grails-app/domain/com/stele/Recargo.groovy
@@ -1,15 +1,18 @@
 package com.stele
 
 class Recargo {
+
   BigDecimal porcentaje
   BigDecimal cantidad
 
   Date dateCreated
   Date lastUpdated
 
-  static constraing = {
-    porcentaje nullable:true, min:0.0
-    cantidad nullable:true, min:0.0
+  Institucion institucion
+
+  static constraints = {
+    porcentaje nullable: true, min:0.0
+    cantidad nullable: true, min:0.0
   }
 
 }
