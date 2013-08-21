3e9980c19aec2a37f9ff033fadafcdbaf0d7c785
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-21 11:32:40 -0500

    coding #117 se crea el metodo que busca los pagos vencidos

--- grails-app/services/com/stele/EstatusPagoService.groovy
+++ grails-app/services/com/stele/EstatusPagoService.groovy
@@ -1,6 +1,22 @@
 package com.stele
 
+import com.stele.Pago
+
 class EstatusPagoService {
 
-  
+  def cambioEstatusVencidoDeUnPago() {
+
+    def listaDePagosVencidos = Pago.withCriteria {
+      ge('fechaDeVencimiento', new Date())
+      eq('estatusDePago', EstatusDePago.CREADO)
+    }
+
+    li staDePagosVencidos.each { pago ->
+      if (pago.estatusDePago == EstatusDePago.CREADO) {
+        pago.estatusDePago = EstatusDePago.VENCIDO
+        pago.save()
+      } 
+    }
+
+  }
 }
