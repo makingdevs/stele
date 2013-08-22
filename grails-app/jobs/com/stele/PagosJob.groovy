270e9b4c74c56d48bf3fdb0428bf1385ee2e0948
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-22 13:55:46 -0500

    coding #115 se inyecta el servicio de pagos para cambiar el estatus de los pagos

--- grails-app/jobs/com/stele/PagosJob.groovy
+++ grails-app/jobs/com/stele/PagosJob.groovy
@@ -1,10 +1,15 @@
 package com.stele
 
 class PagosJob {
+
+  def pagoService
+
     static triggers = {
       cron name: 'pagosVencidos', cronExpression: "0 0 1 * * ?"
     }
 
     def execute() {
+      def resultadoJob = pagoService.buscarPagosConFechasDeVencimientoCumplida()
+      log.debug "Ejecucion del job : $resultadoJob"
     }
 }
