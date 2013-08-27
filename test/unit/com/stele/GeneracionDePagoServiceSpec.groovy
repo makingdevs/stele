5c66b75591cd61f6e91f446fc0cfb9aa508ff9c5
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-27 11:39:35 -0500

    test #130 se crea la prueba para los pagos dobles

--- test/unit/com/stele/GeneracionDePagoServiceSpec.groovy
+++ test/unit/com/stele/GeneracionDePagoServiceSpec.groovy
@@ -185,6 +185,36 @@ class GeneracionDePagoServiceSpec extends Specification {
         "123"  | "concepto" | 1.00  | new Date() + 7     | [1,3,5,10]
   }
 
+  def "Generar un talonario de pagos con pagos doble para una camada"() {
+    setup:
+        Dependiente dependiente = new Dependiente(camada:camada)
+        dependiente.addToHistorialAcademico(new HistorialAcademico())
+        dependiente.save(validate:false)
+      and :
+        CamadaPagoCommand cmd = new CamadaPagoCommand(camada:camada,
+          conceptoDePago:concepto,
+          cantidadDePago:monto,
+          fechaDeVencimiento:fechaDeVencimiento,
+          meses: meses,
+          pagoDoble: pagoDoble
+          )
+      and :
+        def mocks = creoColaboradores()
+      when : 
+        def pagos = service.paraCamadaPagoCommand(cmd)
+        mocks*.verify()
+      then :
+        assert pagos.size() == 6
+        assert pagos.first().id == 1
+        assert pagos.first().conceptoDePago == concepto
+        assert pagos.first().cantidadDePago == monto 
+
+      where : 
+        camada | concepto   | monto | fechaDeVencimiento | meses       | pagoDoble
+        "123"  | "concepto" | 1.00  | new Date() + 7     | [1,3,5,7,9] | [1,5,9]
+
+  }
+
   private def creoColaboradores(){
     Usuario usuario = new Usuario()
 
