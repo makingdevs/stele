ef534a4d943bc563bfca3751c0aacd7143a8a058
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-08-28 13:17:27 -0500

    Coding #139 se crean lcreateCriteri y withCriteria pra verificar cual es el que arroja los dependientes

--- grails-app/controllers/com/stele/CamadaController.groovy
+++ grails-app/controllers/com/stele/CamadaController.groovy
@@ -3,8 +3,6 @@ package com.stele
 class CamadaController {
 
   def show(){
-    println "controller Show"
-    log.info "controller show 1"
     String camada = params.camada
     if(!camada || camada == 'NaC'){
       flash.message = "No hemos identificado el grupo de alumnos!!!"
@@ -12,9 +10,19 @@ class CamadaController {
       return
     }
     def dependientes = Dependiente.findAllByCamada(camada,[fetch:[perfil:'eager',usuario:'eager']])
-    log.info "dependientes : $dependientes"
-    println dependientes
-    log.info "camada : $params.camada  "
+    def dependientesCreateCriteria = Dependiente.createCriteria().list() {
+      'in'('camada', camada)
+      fetchMode('perfil', FM.EAGER)
+      fetchMode('usuario', FM.EAGER)
+    }
+    def dependientesWithCriteria = Dependiente.withCriteria {
+      'in'(camada, camada)
+      fetchMode "perfil", FetchMode.EAGER
+      fetchMode "usuario", FetchMode.EAGER
+    }
+    println "dependientes : $dependientes"
+    println "dependientesCreateCriteria : $dependientesCreateCriteria"
+    println "dependientesWithCriteria : $dependientesWithCriteria"
     [dependientes:dependientes,camada:params.camada]
   }
 }
