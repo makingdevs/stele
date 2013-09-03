3d2eb6971abb9f46ef371c74f48d69833da8bd8e
neodevelop <neodevelop@gmail.com>
2013-09-03 13:26:10 -0500

    coding #152 agregando cantidad y constraint de recargo

--- grails-app/domain/com/stele/EsquemaDePago.groovy
+++ grails-app/domain/com/stele/EsquemaDePago.groovy
@@ -2,9 +2,14 @@ package com.stele
 
 class EsquemaDePago {
 
+  BigDecimal cantidadDePago
   Concepto concepto
   Recargo recargo
 
   static hasMany = [descuentos:Descuento]
+
+  static constraints = {
+    recargo(nullable:true)
+  }
 
 }
