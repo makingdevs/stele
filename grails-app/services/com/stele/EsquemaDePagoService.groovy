214757cadb96fd48be0efdcdf38ddc0da5e24de7
neodevelop <neodevelop@gmail.com>
2013-09-03 14:33:23 -0500

    coding #152 agregando firmas de métodos a esquemaDePagoService

--- grails-app/services/com/stele/EsquemaDePagoService.groovy
+++ grails-app/services/com/stele/EsquemaDePagoService.groovy
@@ -2,7 +2,11 @@ package com.stele
 
 class EsquemaDePagoService {
 
-    def serviceMethod() {
+  def obtenerEsquemaDePago(Long esquemaDePagoServiceId) {
 
-    }
+  }
+
+  def obtenerCantidadDeDescuentoAplicable(EsquemaDePago esquemaDePago) {
+
+  }
 }
