7a23ff5ffb7933bcca4f3573eeb4a5beccc10ed3
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-09-12 14:23:07 -0500

    coding #162 se guarda el numero telefonico del registro

--- grails-app/controllers/com/stele/RegistroController.groovy
+++ grails-app/controllers/com/stele/RegistroController.groovy
@@ -16,12 +16,16 @@ class RegistroController {
     if (!recaptchaService.verifyAnswer(session, request.getRemoteAddr(), params))  {
       flash.error = "Favor de verificar los caracteres ingresados"
       render (view: "index", model:[params:params])
-    } else {
+    } else { 
       Institucion institucion = new Institucion(params.institucion)
       Usuario usuario = new Usuario(params.usuario + [enabled:true])
       Perfil perfil = new Perfil(params.perfil)
+      Telefono telefono = new Telefono()
       if(!Usuario.findByUsername(usuario.username)){
         def rol = Rol.findByAuthority("ROLE_DIRECTOR")
+        telefono.numeroTelefonico = params.numeroTelefonico
+        telefono.save()
+        perfil.addToTelefonos(telefono)
         usuario.perfil = perfil.save()
         usuario.addToInstituciones(institucion)
         usuario.save()
