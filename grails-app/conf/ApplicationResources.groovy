79f2e49d0c3cfa62a3e4b91e3c08764c1f2f68c6
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-09-18 14:51:16 -0500

    coding #161 se agregan las dependencias de css y js para el nuevo dashboard

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -68,4 +68,17 @@ modules = {
 
   }
 
+  assetsDashBoard {
+    resource url: 'css/assets/font-awesome.min.css'
+    resource url: 'css/assets/ace-fonts.css'
+    resource url: 'css/assets/ace.min.css'
+    resource url: 'css/assets/ace-responsive.min.css'
+    resource url: 'css/assets/ace-skins.min.css'
+    resource url: 'js/assets/ace-extra.min.js'
+    resource url: 'js/assets/jquery-2.0.3.min.js'
+    resource url: 'js/assets/jquery.mobile.custom.min.js'
+    resource url: 'js/assets/ace-elements.min.js'
+    resource url: 'js/assets/ace.min.js'
+
+  }
 }
