a2dcf2ea0abf1e4fee104e7f3bf6298761f89ebb
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-09-19 17:51:57 -0500

    coding #161 se invocan a los matodos para guardar solo un dependiente y su tutor

--- grails-app/controllers/com/stele/InscripcionManualController.groovy
+++ grails-app/controllers/com/stele/InscripcionManualController.groovy
@@ -3,13 +3,31 @@ package com.stele
 class InscripcionManualController {
 
 	  def springSecurityService
+    def usuarioService
+    def dependienteService
+    def cicloEscolarService
+    def distribucionInstitucionalService
+    def historialAcademicoService
+
 
 		def inscripcion() {
     	def user = springSecurityService.currentUser
     	[usuario:user]
 		}
 
-		def crearUsuarioCondependiente() {
-			println params
-		}
+		def crearUsuarioCondependiente(InscripcionCommand insc ) {
+      def mapaDomains = [usuario: usuarioService.obtenerUsuarioDesdeCommand(insc),
+                         dependiente:dependienteService.obtenerDependienteDesdeCommand(insc),
+                         cicloEscolar:cicloEscolarService.obtenerCicloEscolarDesdeCommand(insc),
+		                     distribucionInstitucional:distribucionInstitucionalService.obtenerDistribucionInstitucionalDesdeCommand(insc)
+                        ]
+      def usuarioCargado = usuarioService.registrar(mapaDomains.usuario)
+      def dependienteCargado = dependienteService.registrar(mapaDomains.dependiente, usuarioCargado.id)
+      def cicloEscolarCargado = cicloEscolarService.registrar(mapaDomains.cicloEscolar)
+      println cicloEscolarCargado
+      def distribucionInstitucionalCargado = distribucionInstitucionalService.registrar(mapaDomains.distribucionInstitucional, springSecurityService.currentUser.instituciones?.first())
+      def historialAcademico = historialAcademicoService.registrar(historialAcademicoService.preparaHistoricoAcademicoARegistrar(dependienteCargado,distribucionInstitucionalCargado))
+      flash.success = "Se ha creado correctamente el Estudiante" 
+      [usuario: usuarioCargado, dependiente: dependienteCargado, cicloEscolar: cicloEscolarCargado, distribucionInstitucional: distribucionInstitucionalCargado]
+    }
 }
