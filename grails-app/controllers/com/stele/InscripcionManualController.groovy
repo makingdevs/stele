144d163de04a8e9b3ebe6f5bd8c66a45c891a010
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-09-20 13:38:57 -0500

    coding #161 se agrega la camada al dependiente

--- grails-app/controllers/com/stele/InscripcionManualController.groovy
+++ grails-app/controllers/com/stele/InscripcionManualController.groovy
@@ -21,13 +21,21 @@ class InscripcionManualController {
                          cicloEscolar:cicloEscolarService.obtenerCicloEscolarDesdeCommand(insc),
 		                     distribucionInstitucional:distribucionInstitucionalService.obtenerDistribucionInstitucionalDesdeCommand(insc)
                         ]
+      def institucion = springSecurityService.currentUser.instituciones?.first()
+      mapaDomains.dependiente.camada = generarNombreCamada(institucion)
+
       def usuarioCargado = usuarioService.registrar(mapaDomains.usuario)
       def dependienteCargado = dependienteService.registrar(mapaDomains.dependiente, usuarioCargado.id)
       def cicloEscolarCargado = cicloEscolarService.registrar(mapaDomains.cicloEscolar)
-      println cicloEscolarCargado
-      def distribucionInstitucionalCargado = distribucionInstitucionalService.registrar(mapaDomains.distribucionInstitucional, springSecurityService.currentUser.instituciones?.first())
+      def distribucionInstitucionalCargado = distribucionInstitucionalService.registrar(mapaDomains.distribucionInstitucional, institucion.id)
       def historialAcademico = historialAcademicoService.registrar(historialAcademicoService.preparaHistoricoAcademicoARegistrar(dependienteCargado,distribucionInstitucionalCargado))
-      flash.success = "Se ha creado correctamente el Estudiante" 
-      [usuario: usuarioCargado, dependiente: dependienteCargado, cicloEscolar: cicloEscolarCargado, distribucionInstitucional: distribucionInstitucionalCargado]
+      flash.success = "Se ha creado correctamente el Estudiante $dependienteCargado.perfil.nombre $dependienteCargado.perfil.apellidoPaterno $dependienteCargado.perfil.apellidoMaterno" 
+      render(view : "inscripcion" , model:[usuario: springSecurityService.currentUser])
+    }
+
+    def generarNombreCamada(Institucion institucion) {
+      def nombreDeInstitucion = institucion.nombre.size() > 100 ? institucion.nombre.substring(0,80) : institucion.nombre
+      def camadaGenerada = nombreDeInstitucion.replaceAll(" ","_") + "_" + new Date().format("dd_MM_yy_HH_mm")
+      camadaGenerada
     }
 }
