3e8c46fecaf4620648d0591ac13eb67e46c532d7
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-09-24 13:52:03 -0500

    coding #168 se ajusta el job y se agregan los metodos para los recargos y descuentos

--- grails-app/jobs/com/stele/PagosJob.groovy
+++ grails-app/jobs/com/stele/PagosJob.groovy
@@ -2,14 +2,19 @@ package com.stele
 
 class PagosJob {
 
- /* def pagoService
+  def pagoService
 
     static triggers = {
-      cron name: 'pagosVencidos', cronExpression: "0 0 1 * * ?"
+      cron name: 'pagosVencidos', cronExpression: "10 * * * * ?"
     }
 
     def execute() {
-      def resultadoJob = pagoService.buscarPagosConFechasDeVencimientoCumplida()
-      log.debug "Ejecucion del job : $resultadoJob"
-    }*/
+      def resultadoRecargosJob = pagoService.buscarPagosConFechasDeVencimientoCumplida()
+      log.debug "Ejecucion del job Busqueda recargos : $resultadoRecargosJob"
+      def resultadoDescuentoJob = pagoService.buscarPagosCreadosYEnTiempo()
+      log.debug "Ejecucion del job Busqueda descuentos : $resultadoDescuentoJob"
+      //def pagosVencidos = pagoService.cambiarEstatusDePagoAVencido(resultadoRecargosJob)
+      //log.debug "Ejecucion del job Servicio: $pagosVencidos"
+      def pagosConDescuento = pagoService.verificarVijenciaDescuentoYAplicacion(resultadoDescuentoJob)
+    }
 }
