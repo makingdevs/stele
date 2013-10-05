a832618bc37180920a794905a3c1707daa6277e3
says33 <says.rodriguez@gmail.com>
2013-10-05 16:28:10 -0500

    coding deployFix se agregan las dependencias y los modulos para el nuevo layout

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -72,27 +72,73 @@ modules = {
 
   }
 
-  assetsDashBoard {
-    resource url: 'css/assets/font-awesome.min.css'
-    resource url: 'css/assets/ace-fonts.css'
-    resource url: 'css/assets/ace.min.css'
-    resource url: 'css/assets/ace-responsive.min.css'
-    resource url: 'css/assets/ace-skins.min.css'
-    resource url: 'js/assets/ace-extra.min.js'
-    resource url: 'js/assets/ace-elements.min.js'
-    resource url: 'js/assets/ace.min.js'
+  customBootsrapCss{
+    resource url:'assets/css/bootstrap.min.css'
+    resource url:'assets/css/bootstrap-responsive.min.css'
   }
-
-  inscripcionArchivo {
-    resource url: 'js/assets/fuelux/fuelux.wizard.min.js'
-    resource url: 'js/assets/bootbox.min.js'
-    resource url: 'js/assets/jquery.nestable.min.js'
-    resource url: 'js/assets/jquery.dataTables.min.js'
-    resource url: 'js/assets/jquery.dataTables.bootstrap.js'
-    resource url: 'js/assets/jquery-ui-1.10.3.custom.min.js'
-    resource url: 'js/assets/chosen.jquery.min.js'
-    resource url: 'js/assets/jquery.autosize-min.js'
-    resource url: 'js/assets/jquery.inputlimiter.1.3.1.min.js'
-    resource url: 'js/assets/jquery.maskedinput.min.js'
+  customFontAwesomeCss{
+    resource url:'assets/css/font-awesome.min.css'
+  }
+  dropzoneCss {
+    resource url:'assets/css/dropzone.css'
+  }
+  customAceCss{
+    resource url:'assets/css/ace-fonts.css'
+    resource url:'assets/css/ace.min.css'
+    resource url:'assets/css/ace-responsive.min.css'
+    resource url:'assets/css/ace-skins.min.css'
+  }
+  colegioCss {
+    dependsOn 'customBootsrapCss'
+    dependsOn 'customFontAwesomeCss'
+    dependsOn 'dropzoneCss'
+    dependsOn 'customAceCss'
+  }
+  jquery2{
+    resource url:'assets/js/jquery-2.0.3.min.js', wrapper: { s ->
+      def url = s.find(/src.+.js|.css/) - "src=\""
+      """
+        <!--[if !IE]>-->
+        <script type="text/javascript">
+          window.jQuery || document.write("<script src='$url'>"+"<"+"/script>");
+        </script>
+        <!--<![endif]-->
+      """
+    }
+  }
+  jquery1{
+    resource url:'assets/js/jquery-1.10.2.min.js', wrapper: { s ->
+      def url = s.find(/src.+.js|.css/) - "src=\""
+      """
+        <!--[if IE]>
+        <script type="text/javascript">
+         window.jQuery || document.write("<script src='url'>"+"<"+"/script>");
+        </script>
+        <![endif]-->
+      """
+    }
+  }
+  colegioJs {
+    resource url:'assets/js/ace-extra.min.js'
+    resource url:'assets/js/bootstrap.min.js'
+    resource url:'assets/js/ace-elements.min.js'
+    resource url:'assets/js/ace.min.js'
+    resource url:'assets/js/fuelux/fuelux.wizard.min.js'
+    resource url:'assets/js/bootbox.min.js'
+    resource url:'assets/js/dropzone.min.js'
+    resource url:'assets/js/jquery.nestable.min.js'
+    resource url:'assets/js/jquery.dataTables.min.js'
+    resource url:'assets/js/jquery.dataTables.bootstrap.js'
+    resource url:'assets/js/jquery-ui-1.10.3.custom.min.js'
+    resource url:'assets/js/chosen.jquery.min.js'
+    resource url:'assets/js/jquery.autosize-min.js'
+    resource url:'assets/js/jquery.inputlimiter.1.3.1.min.js'
+    resource url:'assets/js/jquery.maskedinput.min.js'
+  }
+  colegio {
+    dependsOn 'colegioCss'
+    dependsOn 'jquery2'
+    dependsOn 'jquery1'
+    dependsOn 'colegioJs'
   }
 }
