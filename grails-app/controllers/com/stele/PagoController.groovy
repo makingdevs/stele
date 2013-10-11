a2da9031dbd2e2432409560ea1f07e873ecf9498
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-11 13:52:31 -0500

    coding #170 se cambian los nombres de los metodos del service a causa de refactorin y mejoras funcionales

--- grails-app/controllers/com/stele/PagoController.groovy
+++ grails-app/controllers/com/stele/PagoController.groovy
@@ -6,18 +6,16 @@ class PagoController {
 
   def springSecurityService
   def pagoService
+  def historialAcademicoService
 
   def index() { 
-    def pagosDeUsuario = pagoService.obtenerPagosDeLosDependientesEnBaseAUsuario(springSecurityService.currentUser)
-    [pagosDeUsuario: pagosDeUsuario.pagos, usuario: springSecurityService.currentUser]
+    def pagosDeUsuario = pagoService.estadoDeCuentaUsuario(springSecurityService.currentUser)
+    [pagosDeUsuario: pagosDeUsuario, usuario: springSecurityService.currentUser]
   }
 
-  def pagosDeUnaInstitucion() {
-    def pago = pagoService.obtenerPagosDeUnaInstitucion(springSecurityService.currentUser)
-   [
-   pagosInstitucion: Pago.findAllByHistorialAcademicoInList(pago.historial), 
-   usuario: springSecurityService.currentUser, 
-   pagosCount: pago.dependiente]
+  def mostrarPagosAsociadosALaInstitucionEnBaseAHistorialesAcademicos() {
+   def pagosAsociadosAInstitucion = historialAcademicoService.obtenerHistorilesAcademicosYPagosDeUnaInstitucion(springSecurityService.currentUser)
+   render (view: "pagosAsociadosAUnaInstitucion", model:[pagosInstitucion:pagosAsociadosAInstitucion, usuario:springSecurityService.currentUser])
   }
 
 }
