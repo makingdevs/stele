acc333da1466e2cdcaea90a039a572d3723d54cb
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-14 13:04:20 -0500

    coding #170 se ajusta la prueba del parseo y se centraliza las variables de los objetos

--- test/unit/com/stele/WrapperCommandServiceSpec.groovy
+++ test/unit/com/stele/WrapperCommandServiceSpec.groovy
@@ -7,34 +7,43 @@ import spock.lang.Specification
 import com.payable.GrupoPagoCommand
 
 @TestFor(WrapperCommandService)
-@Mock(Dependiente)
+@Mock([Dependiente, Institucion])
 class WrapperCommandServiceSpec extends Specification {
 
   def "Generar parseo de camadaCommad a GrupoCommand directo"() {
     given:
+      Institucion institucion = new Institucion()
+      institucion.nombre = "la del moño colorado"
+      institucion.save(validate:false)
+    and:
       Dependiente dependiente1 = new Dependiente()
       dependiente1.matricula = "M224589"
-      dependiente1.camada = "Making_Devs"
+      dependiente1.camada = camada
       dependiente1.save(validate:false)
     and:
       Dependiente dependiente2 = new Dependiente()
       dependiente2.matricula = "M221454"
-      dependiente2.camada = "Making_Devs"
+      dependiente2.camada = camada
       dependiente2.save(validate:false)
     and:
       CamadaPagoCommand cpc = new CamadaPagoCommand()
-      cpc.camada = "Making_Devs"
-      cpc.conceptoDePago = "Inscripcion"
-      cpc.cantidadDePago = 15000
-      cpc.fechaDeVencimiento = new Date() + 30
-      cpc.diasPreviosParaCancelarDescuento = 5
-      cpc.meses = [2,4,6,8]  
-      cpc.pagoDoble = [2,8]
+      cpc.camada = camada
+      cpc.conceptoDePago = conceptoDePago
+      cpc.cantidadDePago = cantidadDePago
+      cpc.fechaDeVencimiento = fechaDeVencimiento
+      cpc.diasPreviosParaCancelarDescuento = diasPrevios
+      cpc.meses = meses
+      cpc.pagoDoble = pagoDoble
       cpc.listaDependientes = "[${dependiente1.id}, ${dependiente2.id}]"
     when: "verificar que el parseo de los command es satisfactorio"
-      def grupoCommand = service.generarParseoDeCamadaPagoCommandAGrupoPagoCommand(cpc)
+      def grupoCommand = service.generarParseoDeCamadaPagoCommandAGrupoPagoCommand(cpc, institucion)
     then:
-      assert grupoCommand instanceof GrupoPagoCommand    
+      assert grupoCommand instanceof GrupoPagoCommand  
+      assert grupoCommand.cantidadDePago == cantidadDePago
+      assert grupoCommand.organizacion.id  == institucion.id
+    where:
+     camada        |cantidadDePago | conceptoDePago | fechaDeVencimiento | diasPrevios | meses      | pagoDoble 
+     "Making_Devs" | 15000         | "Inscripcion"  | new Date() + 30    |  6          | [2,4,6,8]  | [2,8]
   }
 
 }
