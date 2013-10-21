636b2e515823aebd798624e9b46185c70f5e2b51
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-21 14:31:48 -0500

    coding #170 se ajustan los metodos para el nuevo uso del pplugin de payable

--- grails-app/controllers/com/stele/EsquemaDePagoController.groovy
+++ grails-app/controllers/com/stele/EsquemaDePagoController.groovy
@@ -24,11 +24,12 @@ class EsquemaDePagoController {
       return
     } 
     def grupoPagoCommand = wrapperCommandService.generarParseoDeCamadaPagoCommandAGrupoPagoCommand(cpc, springSecurityService.currentUser.instituciones?.first())
-    def esquemaDePago = esquemaDePagoService.buscarOSalvarEsquemaDePago(grupoPagoCommand)
-    def pagosGenerados = generacionDePagoService.generaPagoParaGrupo(grupoPagoCommand)
-    def pagos = verificarExistenciaDeFechaDeVencimientoEnDescuentoParaObtenerPagosConDescuentosAplicables(grupoPagoCommand, esquemaDePago, pagosGenerados)
+    def pagos = generacionDePagoService.generaPagoParaGrupo(grupoPagoCommand)
+    if (grupoPagoCommand.descuentoIds) {
+      def esquemaDePago = esquemaDePagoService.buscarOSalvarEsquemaDePago(grupoPagoCommand)
+      pagos = verificarExistenciaDeFechaDeVencimientoEnDescuentoParaObtenerPagosConDescuentosAplicables(grupoPagoCommand, esquemaDePago, pagos)
+    }
     flash.pago = pagos
-    flash.success = "Bien Hecho"
     redirect action:"muestraPagosDeCamada",params: params + [camada:cpc.camada]
   }
 
@@ -39,13 +40,13 @@ class EsquemaDePagoController {
 
   private def verificarExistenciaDeFechaDeVencimientoEnDescuentoParaObtenerPagosConDescuentosAplicables(GrupoPagoCommand grupoPagoCommand,EsquemaDePago esquemaDePago, List pagos) {
     def listaDePagos
-    grupoPagoCommand.descuentoIds.each { idDescuento ->
+    def listaDeDescuentos = grupoPagoCommand.descuentoIds.first()
+    listaDeDescuentos = listaDeDescuentos.replace('[','')?.replace(']','')?.split(',')
+    listaDeDescuentos.each { idDescuento ->
       def descuentoAplicable 
-      def descuento = Descuento.get(idDescuento)
-      if (descuento.fechaDeVencimiento)
-        descuentoAplicable = descuentoAplicableService.generarParaPagoConVencimiento(descuento.fechaDeVencimiento, descuento.id)
-      else 
-        descuentoAplicable = descuentoAplicableService.generarParaPagoConEsquemaDePagoConFechaReferencia(esquemaDePago.id, grupoPagoCommand.fechaDeVencimiento)
+      def descuento = Descuento.get(idDescuento.toLong())
+      
+      descuentoAplicable = descuentoAplicableService.generarParaPagoConEsquemaDePagoConFechaReferencia(esquemaDePago.id, grupoPagoCommand.fechaDeVencimiento)
 
       listaDePagos = asignarDescuntosAplicablesAlosPagos(descuentoAplicable, pagos)
 
