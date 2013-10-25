50448b73b148747e82cd4b77db6c0c1ad026521d
says33 <says.rodriguez@gmail.com>
2013-10-24 23:37:40 -0500

    codign #182 se ajusta la creacion de descuentos en base a la fechaVencimiento

--- grails-app/controllers/com/stele/DescuentoController.groovy
+++ grails-app/controllers/com/stele/DescuentoController.groovy
@@ -15,16 +15,11 @@ class DescuentoController {
   def nuevo() {
     Descuento descuento = findOrSaveDescuentoWithParams(params)
     def descuentosIds = [descuento.id]
-    def descuentoConFechaDeVencimiento = params.descuentoConFecha?.replace('[','')?.replace(']','')?.split(',') ?: ""
     if(params.descuentosIds) {
       def listaDescuentos = params.descuentosIds?.replace('[','')?.replace(']','')?.split(',') ?: []
       descuentosIds += listaDescuentos.collect{ s -> Long.valueOf(s) }
     }
-    println descuentoConFechaDeVencimiento
-    if (params.diasPreviosParaCancelarDescuento.equals("1")){
-      descuentoConFechaDeVencimiento += descuento?.id + ":" + params.fechaDeVencimiento 
-    }
-    render template:"/descuento/list", model:[descuentos:Descuento.getAll(descuentosIds), descuentosIds:descuentosIds,descuentosVencimiento:descuentoConFechaDeVencimiento]
+    render template:"/descuento/list", model:[descuentos:Descuento.getAll(descuentosIds), descuentosIds:descuentosIds]
   }
 
   def obtenerDescuentosInstitucion() {
@@ -39,9 +34,25 @@ class DescuentoController {
     if(!descuento) {
       descuento = new Descuento(params)
       descuento.organizacion = springSecurityService.currentUser.instituciones?.first()  
+      if (!params.diasPreviosParaCancelarDescuento){
+        def diasAntes = getLastDayOfMothByDate(params.fechaDeVencimiento)
+        descuento.diasPreviosParaCancelarDescuento = diasAntes
+      }
       descuento.save(flush:true)
     }
     descuento
+  }
+
+  private Integer getLastDayOfMothByDate(Date fechaDeVencimiento){
+    Calendar cala = Calendar.getInstance()    
+    cala.setTime(fechaDeVencimiento)
+    def year = cala.get(Calendar.YEAR)
+    def month = cala.get(Calendar.MONTH)
+    def day = cala.get(Calendar.DAY_OF_MONTH)
+    Calendar calendario = GregorianCalendar.instance
+    calendario.set(year,month,day)  
+    def lastDay = calendario.getActualMaximum(GregorianCalendar.DAY_OF_MONTH)
+    def dias = lastDay - day
   }
 
 }
