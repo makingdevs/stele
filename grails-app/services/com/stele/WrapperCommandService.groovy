34ae7bfaddc731bda3ead7d9017d55a409f06ae8
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-28 12:33:51 -0600

    coding #182 se genera el parseo de String a Date para la fechaDevencimiento

--- grails-app/services/com/stele/WrapperCommandService.groovy
+++ grails-app/services/com/stele/WrapperCommandService.groovy
@@ -9,7 +9,7 @@ class WrapperCommandService {
     gcp.recargoId = camadaPagoCommand.recargoid?.first()?.toLong()
     gcp.cantidadDePago = camadaPagoCommand.cantidadDePago ?: camadaPagoCommand.cantidadDePagoRecurrente
     gcp.conceptoDePago = camadaPagoCommand.conceptoDePago ?: camadaPagoCommand.conceptoDePagoRecurrente
-    gcp.fechaDeVencimiento = camadaPagoCommand.fechaDeVencimiento
+    gcp.fechaDeVencimiento = new Date().parse("dd/MM/yyyy", camadaPagoCommand.fechaDeVencimiento)
     gcp.descuentoIds = camadaPagoCommand.descuentos
     gcp.organizacion = institucion
     gcp.diasVencimientoPago = camadaPagoCommand.diasVencimientoPago
