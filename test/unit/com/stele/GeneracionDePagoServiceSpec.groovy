900a380c15d48a6ce0fa1604c3041a2ce4608cc9
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-28 13:35:07 -0600

    coding #182 se ajustan los valores para las pruebas

--- test/unit/com/stele/GeneracionDePagoServiceSpec.groovy
+++ test/unit/com/stele/GeneracionDePagoServiceSpec.groovy
@@ -203,7 +203,7 @@ class GeneracionDePagoServiceSpec extends Specification {
         def pagos = service.generaPagoParaGrupo(gpc)
         mocks*.verify()
       then :
-        assert pagos.size() == 5
+        assert pagos.size() == 4
         assert pagos.first().id > 0
         assert pagos.first().conceptoDePago == concepto
         assert pagos.first().cantidadDePago == monto 
@@ -236,7 +236,7 @@ class GeneracionDePagoServiceSpec extends Specification {
       def pagos = service.generaPagoParaGrupo(gpc)
       mocks*.verify()
     then :
-      assert pagos.size() == 5
+      assert pagos.size() == 6
       assert pagos.first().id == 1
       assert pagos.first().conceptoDePago == concepto
       pagos.each { pago ->
