5637543cec25b451bd62fadf7d8552b86048a840
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-29 08:58:27 -0600

    coding #170 se elimina el println innecesario

--- grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
+++ grails-app/controllers/com/stele/ProcesamientoMasivoController.groovy
@@ -5,7 +5,6 @@ class ProcesamientoMasivoController {
   def procesamientoMasivoService
 
   def index() {
-    println flash.proceso = params.cobro
     def listaDeMapaDeDominios = flash.listaDeMapaDeDominios
     def institucion = Institucion.get(params?.long("institucionId"))
     def nombreDeInstitucion = institucion.nombre.size() > 100 ? institucion.nombre.substring(0,80) : institucion.nombre
