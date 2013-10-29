efbb81336d4196673da5306ea56e079d7d8d9b1d
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-10-29 08:57:49 -0600

    coding #170 se ajusta los datos del regreso del metodo para la variable flash

--- grails-app/controllers/com/stele/InscripcionManualController.groovy
+++ grails-app/controllers/com/stele/InscripcionManualController.groovy
@@ -13,8 +13,8 @@ class InscripcionManualController {
 		def crearUsuarioCondependiente(InscripcionCommand insc ) {
       
       def institucion = springSecurityService.currentUser.instituciones?.first()
-      def registroAlumnoTutor = inscripcionManualService.generarRegistroDeAlumnoYTutor(insc, inscripcion)
-      flash.success = "Se ha creado correctamente el Estudiante $dependienteCargado.perfil.nombre $dependienteCargado.perfil.apellidoPaterno $dependienteCargado.perfil.apellidoMaterno" 
+      def registroAlumnoTutor = inscripcionManualService.generarRegistroDeAlumnoYTutor(insc, institucion)
+      flash.success = "Se ha creado correctamente el Estudiante $registroAlumnoTutor.dependiente.perfil.nombre $registroAlumnoTutor.dependiente.perfil.apellidoPaterno $registroAlumnoTutor.dependiente.perfil.apellidoMaterno" 
       render(view : "inscripcion" , model:[usuario: springSecurityService.currentUser])
     }
 
