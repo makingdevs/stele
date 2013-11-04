1c2f01a4c94df19f5a1530f17dee5d99dca15737
says33 <says.rodriguez@gmail.com>
2013-11-03 19:51:53 -0600

    coding #184 se ajusta el job para  el vencimiento de pagos

--- grails-app/jobs/com/stele/PagosJob.groovy
+++ grails-app/jobs/com/stele/PagosJob.groovy
@@ -1,21 +1,17 @@
 package com.stele
 
+import com.payable.*
+
 class PagosJob {
 
   def pagoService
 
     static triggers = {
-      cron name: 'pagosVencidos', cronExpression: "0 0 0 * * ?"
+      cron name: 'pagosVencidos', cronExpression: "0 0 1 * * ?"
     }
 
     def execute() {
-      def resultadoRecargosJob = pagoService.buscarPagosConFechasDeVencimientoCumplida()
+      def resultadoRecargosJob = pagoService.exprirarPagosYCalcularRecargo()
       log.debug "Ejecucion del job Busqueda recargos : $resultadoRecargosJob"
-      def resultadoDescuentoJob = pagoService.buscarPagosCreadosYEnTiempo()
-      log.debug "Ejecucion del job Busqueda descuentos : $resultadoDescuentoJob"
-      //def pagosVencidos = pagoService.cambiarEstatusDePagoAVencido(resultadoRecargosJob)
-      //log.debug "Ejecucion del job Servicio: $pagosVencidos"
-      def pagosConDescuento = pagoService.verificarVigenciaDescuentoYAplicacion(resultadoDescuentoJob)
-      log.debug "Resultado de la ejecucion de verificarVigenciaDescuentoYAplicacion : $pagosConDescuento"
     }
 }
