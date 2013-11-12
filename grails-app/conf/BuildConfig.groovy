9b5e8acd8230c5495d40515e03a1cb67d91f9baf
felipe <fjuarezmurillo@synergyj.com>
2013-11-12 11:21:56 -0600

    agregando el group id al payable

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -70,6 +70,6 @@ grails.project.dependency.resolution = {
 
         compile ':cache:1.0.1'
         compile 'com.makingdevs:profile:latest.integration'
-        compile ':payable:latest.integration'
+        compile 'com.payable:payable:latest.integration'
     }
 }
