d446ac87a5fda11198afc75542d1ae1f2983cd8b
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-11-13 15:50:30 -0600

    coding #187 se agrega metodo para separar los pagos por dependiente

--- grails-app/controllers/com/stele/EstadoDeCuentaController.groovy
+++ grails-app/controllers/com/stele/EstadoDeCuentaController.groovy
@@ -5,16 +5,27 @@ class EstadoDeCuentaController {
 
   def springSecurityService
   def pagoService
+  def dependienteService
 
   def show() {
     def usuarioActual = springSecurityService.currentUser
     def estadoDeCuentaUsuario = pagoService.estadoDeCuentaUsuario(springSecurityService.currentUser)
-    log.debug estadoDeCuentaUsuario
+    def dependientes = dependienteService.obnerDependientesPorUsuario(usuarioActual)
+    separarPagosPorDependiente(estadoDeCuentaUsuario, dependientes.dependientes)
     [usuarioActual : usuarioActual, 
      pagosVencidos:estadoDeCuentaUsuario.pagosVencidos,
      pagosEnTiempo:estadoDeCuentaUsuario.pagosEnTiempo,
      pagosPorRealizar:estadoDeCuentaUsuario.pagosPorRealizar,
-     pagosMensuales:estadoDeCuentaUsuario.pagoMensual]
+     pagosMensuales:estadoDeCuentaUsuario.pagoMensual,
+     dependiente: dependientes.dependientes,
+     perfil: dependientes.perfiles]
   }
+
+  def separarPagosPorDependiente(def estadoDeCuentaUsuario, def dependientes) {
+    dependientes.each{ dependiente ->
+      println dependiente.pagos*.id
+    }
+
+  } 
 
 }
