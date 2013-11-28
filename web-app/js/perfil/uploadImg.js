ac0e4f7fbdd37eed0d541bcff33f41be3c62982e
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2013-11-28 12:38:16 -0600

    coding #191 ajuste de los script para el dropzone, apuntando al id del hidden y del input

--- web-app/js/perfil/uploadImg.js
+++ web-app/js/perfil/uploadImg.js
@@ -29,42 +29,42 @@ $('#user-profile-1')
 });
 
 $(function() {
-    var urlValue = $("input#img1").val() 
-      Dropzone.options.dropzone = {
-      url : urlValue,
+    var urlValue1 = $("input#img1").val()
+    $("#perfil1").dropzone({
+      url : urlValue1,
       maxFilesize : .5,
       addRemoveLinks : false
-    };
+    });
 });
 $(function() {
-    var urlValue = $("input#img2").val() 
-      Dropzone.options.dropzone = {
-      url : urlValue,
+    var urlValue2 = $("input#img2").val() 
+    $("#perfil2").dropzone({
+      url : urlValue2,
       maxFilesize : .5,
       addRemoveLinks : false
-    };
+    });
 });
 $(function() {
-    var urlValue = $("input#img3").val() 
-      Dropzone.options.dropzone = {
-      url : urlValue,
+    var urlValue3 = $("input#img3").val() 
+    $("#perfil3").dropzone({
+      url : urlValue3,
       maxFilesize : .5,
       addRemoveLinks : false
-    };
+    });
 });
 $(function() {
-    var urlValue = $("input#img4").val() 
-      Dropzone.options.dropzone = {
-      url : urlValue,
+    var urlValue4 = $("input#img4").val() 
+    $("#perfil4").dropzone({
+      url : urlValue4,
       maxFilesize : .5,
       addRemoveLinks : false
-    };
+    });
 });
 $(function() {
-    var urlValue = $("input#img5").val() 
-      Dropzone.options.dropzone = {
-      url : urlValue,
+    var urlValue5 = $("input#img5").val() 
+    $("#perfil5").dropzone({
+      url : urlValue5,
       maxFilesize : .5,
       addRemoveLinks : false
-    };
+    });
 });
