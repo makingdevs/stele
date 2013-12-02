fffd7b9c75d44882ed4c9b213b246f57f872a749
says33 <says.rodriguez@gmail.com>
2013-12-01 21:22:06 -0600

    coding #192 se crea el archivo con las modificaciones para cuentas bancarias en el dbmigration

---
+++ grails-app/migrations/add-relation-account-transactionBanking.groovy
@@ -0,0 +1,59 @@
+databaseChangeLog = {
+
+	changeSet(author: "says (generated)", id: "1385845466765-1") {
+		sql("set storage_engine=InnoDB;")
+		createTable(tableName: "cuentas_bancarias") {
+			column(autoIncrement: "true", name: "id", type: "bigint") {
+				constraints(nullable: "false", primaryKey: "true", primaryKeyName: "cuentas_bancaPK")
+			}
+
+			column(name: "version", type: "bigint") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "beneficiario", type: "varchar(80)") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "cuenta_interbancaria", type: "decimal(19,2)")
+
+			column(name: "date_created", type: "datetime") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "last_updated", type: "datetime") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "nombre_de_banco", type: "varchar(60)") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "numero_de_cuenta", type: "decimal(19,2)") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "organizacion_id", type: "bigint") {
+				constraints(nullable: "false")
+			}
+
+			column(name: "referencia", type: "varchar(120)")
+
+			column(name: "tipo_transferencia", type: "varchar(255)") {
+				constraints(nullable: "false")
+			}
+		}
+	}
+
+	changeSet(author: "says (generated)", id: "1385845466765-3") {
+		createIndex(indexName: "FKD9D2BCC2418DE2A1", tableName: "cuentas_bancarias") {
+			column(name: "organizacion_id")
+		}
+	}
+
+	changeSet(author: "says (generated)", id: "1385845466765-2") {
+		addForeignKeyConstraint(baseColumnNames: "organizacion_id", baseTableName: "cuentas_bancarias", constraintName: "FKD9D2BCC2418DE2A1", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "organizacion", referencesUniqueColumn: "false")
+	}
+
+
+}
