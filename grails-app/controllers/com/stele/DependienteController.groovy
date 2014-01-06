792e4fc43e415713c54afbf9322d5d635c2438c4
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-05 18:50:29 -0600

    coding #223 se elimina logica de la busqueda por atributos escolares

--- grails-app/controllers/com/stele/DependienteController.groovy
+++ grails-app/controllers/com/stele/DependienteController.groovy
@@ -76,67 +76,6 @@ class DependienteController {
       }
       render (view:"dependientesDetalleList", model:[dependiente:historialAcademico])
     }
-    def ajaxTurnoANivel() {
-      def turno = Turno.find{it.value == params.turno}
-      def niveles = DistribucionInstitucional.withCriteria {
-        eq('turno', turno)
-          projections {
-            groupProperty('nivelDeEstudio')
-          }
-      }
-      def turnos = flash.turnos
-      def nivel=null
-      flash.turnos = turnos
-      flash.turno = turno
-      flash.niveles = niveles
-      render template:'busquedaForm', model:[turnos:turnos,turno:turno,niveles:niveles,nivel:nivel]
-    }
-
-    def ajaxNivelAGrado() {
-      def nivel = NivelDeEstudio.find{it.value == params.nivel}
-      def grados = DistribucionInstitucional.withCriteria {
-        eq('turno', flash.turno)
-        eq('nivelDeEstudio', nivel)
-           projections {
-            groupProperty('grado')
-           }
-      }
-      def turnos = flash.turnos
-      def turno = flash.turno
-      def niveles = flash.niveles
-      flash.turnos = turnos
-      flash.turno = turno
-      flash.niveles = niveles
-      flash.grados = grados
-      flash.nivel = nivel
-      render template:'busquedaForm', model:[turnos:turnos,turno:turno,niveles:niveles,nivel:nivel, grados:grados, grado:null]
-    }
-
-    def ajaxGradoAGrupo() {
-      def grado = params.grado
-      def grupos = DistribucionInstitucional.withCriteria {
-        eq('turno', flash.turno)
-        eq('nivelDeEstudio', flash.nivel)
-        eq('grado', grado.toInteger())
-           projections {
-            groupProperty('grupo')
-           }
-      }
-      def turnos = flash.turnos
-      def turno = flash.turno
-      def niveles = flash.niveles
-      def nivel = flash.nivel
-      def grados = flash.grados
-      flash.turnos = turnos
-      flash.turno = turno
-      flash.niveles = niveles
-      flash.nivel = nivel
-      flash.grados = grados
-      flash.grado = grado
-      render template:'busquedaForm', model:[turnos:turnos,turno:turno,niveles:niveles,nivel:nivel, grados:grados, grado:grado, grupos:grupos, grupo:null]
-    }
-
-    def ajaxGrupo() { }
 
     def busquedaDependienteParaGenerarPago() {
       def searchResult = searchableService.search(params.nombreDependiente)
@@ -150,6 +89,7 @@ class DependienteController {
 
     def buscarDependienteInscripcion() {
       def dependientesCoincidentes = searchableService.search(params.nombreDependienteBusqueda)
-      [dependiente: dependientesCoincidentes.result ?: ""]
+      render template:'/inscripcionManual/listaDependientes', model:[dependientes: dependientesCoincidentes.results]
     }
+    
 }
