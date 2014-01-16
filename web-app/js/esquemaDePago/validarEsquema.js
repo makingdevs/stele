16a94bb7db4fdf6279c7824fe8be057faec4f7eb
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-16 17:55:04 -0600

    coding #236 se agrega la funcion para validar los descuentos en esquema de pagos

--- web-app/js/esquemaDePago/validarEsquema.js
+++ web-app/js/esquemaDePago/validarEsquema.js
@@ -42,3 +42,52 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $("#descuentosForm").validate({
+    errorPlacement: function(error, element) {
+      $(element).parent().parent().parent().addClass("error");
+      error.addClass("help-inline").appendTo(element.parent());
+      error.insertAfter(element);
+    },
+    success: function(element) {
+        $(element).parent().parent().parent().addClass("success");
+    },
+    highlight: function(element, errorClass, validClass){
+      $(element).parent().parent().parent().addClass(errorClass).removeClass(validClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).parent().parent().parent().removeClass(errorClass).addClass(validClass);
+    },
+    rules: {
+      'nombredescuento': {
+        required: true
+      },
+      'cantidad': {
+        number: true
+      },
+      'porcentaje': {
+        number: true
+      },
+      'diasPreviosParaCancelarDescuento': {
+        required: true
+      }
+    },
+    messages: {
+      'nombredescuento': {
+        required: "Se requiere un nombre para el descuento" 
+      },
+      'cantidad': {
+        number: "solo se aceptan numeros"
+      },
+      'porcentaje': {
+        number: "solo se aceptan numeros"
+      },
+      'diasPreviosParaCancelarDescuento': {
+        required: "Se requiere seleccionar un numero de dias"
+      }
+    },
+      validClass: "success",
+      errorClass: "error",
+      errorElement: "span"
+  });
+});
