e2605c68aa2a36eef17d068e95877619252b90c7
Sergio Rodriguez Duran <says.rodriguez@gmail.com>
2014-01-21 19:50:09 -0600

    coding #249 se agrega elemento css faltante

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -69,6 +69,7 @@ modules = {
     resource url:'new/assets/plugins/flexslider/flexslider.css'
     resource url:'new/assets/plugins/bxslider/jquery.bxslider.css'
     resource url:'new/assets/plugins/horizontal-parallax/css/horizontal-parallax.css'
+    resource url:'new/assets/css/pages/page_pricing.css'
     resource url:'new/assets/css/themes/default.css'
   }
 
