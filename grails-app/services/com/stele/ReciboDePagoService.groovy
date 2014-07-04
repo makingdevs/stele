6e3ada0169e05b3c565818f97ccc94770ce56609
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-04 16:29:11 -0500

    coding #266 cambiando las pruebas

--- grails-app/services/com/stele/ReciboDePagoService.groovy
+++ grails-app/services/com/stele/ReciboDePagoService.groovy
@@ -1,13 +1,22 @@
 package com.stele
 
 import grails.transaction.Transactional
+import com.payable.Pago
 
 @Transactional
 class ReciboDePagoService {
+  
+  def dependienteService
 
   def obtenerDatosReciboDePago(pagoId){
     def pago = Pago.get(pagoId)
-    new ComprobantePagoCommand() 
+    def dependientes = dependienteService.obtenerDependientesPorPagos([pago])
+
+    new ComprobantePagoCommand(cantidadPago:pago.cantidadDePago,
+                               conceptoPago:pago.conceptoDePago,
+                               nombreAlumno = dependientes[0].perfil.nombre)
+                               //apellidoPaternoAlumno = dependiente.pefil.apellidoPaterno,
+                               //apellidoMaternoAlumno = dependiente.perfil.apellidoMaterno) 
   }
 
 }
--- test/unit/com/stele/ReciboDePagoServiceSpec.groovy
+++ test/unit/com/stele/ReciboDePagoServiceSpec.groovy
@@ -5,7 +5,7 @@ import org.junit.*
 import spock.lang.Specification
 import spock.lang.Unroll
 import com.payable.Pago
-import com.
+import com.makingdevs.Perfil
 
 @TestFor(ReciboDePagoService)
 @Mock([Pago,Dependiente,Perfil])
@@ -13,6 +13,7 @@ class ReciboDePagoServiceSpec extends Specification {
   
   def "Obtener datos del recibo de Pago"(){
     given:"Se tiene un pago conciliado de un dependiente"
+      def pagoConciliado = new Pago(conceptoDePago:"Inscripcion",cantidadDePago:900).save(validate:false);
       def perfilTutor = new Perfil(nombre:"Silvia",
                                    apellido_paterno:"García",
                                    apellido_materno:"Campos").save(validate:false)
@@ -20,14 +21,21 @@ class ReciboDePagoServiceSpec extends Specification {
       def perfilAlumno = new Perfil(nombre:"Gamaliel",
                                     apellido_paterno:"Jimenez",
                                     apellido_materno:"Garcia").save(validate:false)
-      def dependiente = new Dependiente()
-      def pagoConciliado = new Pago(conceptoDePago:"Inscripcion",cantidadDePago:900);
-      pagoConciliado.save(validate:false)
+      def dependiente = new Dependiente(perfil:perfilAlumno,pagos:[pagoConciliado]).save(validate:false)
+      def dependienteList = [dependiente]
+      Dependiente.metaClass.static.withCriteria = {  dependienteList }
+    and: 
+      def dependienteServiceMock = mockFor(DependienteService)
+      dependienteServiceMock.demand.obtenerDependientesPorPagos{ listaPagos ->}
+      service.dependienteService = dependienteServiceMock.createMock()
     when:
       ComprobantePagoCommand datosReciboDePago = service.obtenerDatosReciboDePago(pagoConciliado.id);
     then:
       datosReciboDePago != null      
-      datosReciboDePago.cantidadPago = 900
+      datosReciboDePago.nombreAlumno == "Gamaliel"
+      //datosReciboDePago.apellidoPaternoAlumno == "Jimenez"
+      //datosReciboDePago.apellidoMaternoAlumno == "Garcia"
+      datosReciboDePago.cantidadPago == 900
       datosReciboDePago.conceptoPago == "Inscripcion"
   }
 }
