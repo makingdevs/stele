b8c44124239ce40cdb4ec8b95c134ab6d77045f0
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-07 11:56:26 -0500

    coding #273 se cambia version de plugin de payable

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -87,7 +87,7 @@ grails.project.dependency.resolution = {
     runtime ":resources:1.2.7"
 
     compile 'com.makingdevs:profile:0.1.7'
-    compile('com.payable:payable:0.2.24'){
+    compile('com.payable:payable:0.2.25'){
       exclude "profile"
     }
   }
