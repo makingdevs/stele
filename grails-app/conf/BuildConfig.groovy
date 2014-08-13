dc4fdee72d2933e1aae44f2ffa56343b0b06dd33
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-13 09:46:07 -0500

    coding #298 upgrade de la versión del plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -87,7 +87,7 @@ grails.project.dependency.resolution = {
     runtime ":resources:1.2.7"
 
     compile 'com.makingdevs:profile:0.1.7'
-    compile('com.payable:payable:0.2.27'){
+    compile('com.payable:payable:0.2.28'){
       exclude "profile"
     }
   }
