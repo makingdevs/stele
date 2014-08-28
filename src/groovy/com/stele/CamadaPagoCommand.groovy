a39e0766b9145352ea1ca82d89b3b91efeb49636
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-08-28 10:48:52 -0500

    coding #295 agregando fechas al command object

--- src/groovy/com/stele/CamadaPagoCommand.groovy
+++ src/groovy/com/stele/CamadaPagoCommand.groovy
@@ -17,6 +17,7 @@ class CamadaPagoCommand {
   String[] recargoid
   String[] meses
   String[] pagoDoble
+  List<String> expiracionDescuento 
   String listaDependientes
   String descuentoConFecha
   String idRecargo
