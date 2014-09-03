b205530d64a7fba8f318aeefcca6118ecb4f3199
egjimenezg <egjimenezg@gmail.com>
2014-09-03 11:10:44 -0500

    coding #295 agregando función para el formulario de descuentos

--- web-app/js/paraCamada/descuento.js
+++ web-app/js/paraCamada/descuento.js
@@ -2,9 +2,12 @@ window.Descuento = (function(){
   
   Descuento.prototype.nombreDescuento = '';
   Descuento.prototype.descuentos = [];  
+  Descuento.prototype.form = ' ';
 
   function Descuento(selectores){
     this.nombreDescuento = selectores.nombreDescuento;
+    this.form = selectores.form;
+    this.initFormAction();
     this.initTypeaheadParaDescuento();
   }
 
@@ -29,7 +32,25 @@ window.Descuento = (function(){
           });
       }
     });
-
   }
+
+  Descuento.prototype.initFormAction = function(){
+    var that = this;
+    this.form.submit(function(event){
+      event.stopPropagation();
+      
+      $.ajax({
+        type: "POST",
+        url:$(this).attr("action"),
+        data:$(this).serialize(),
+        success: function(data){
+          $(".descuentoCreado").html();
+        }  
+      });
+
+      return false
+    });  
+  }
+
   return Descuento;
 })();
