eaf22eb463d94eef2b0fb9a2f4a6cc0e7cb4d432
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-05 13:14:21 -0500

    coding #295 modificando controller para evitar que se guarden recargos duplicados

--- grails-app/controllers/com/stele/RecargoController.groovy
+++ grails-app/controllers/com/stele/RecargoController.groovy
@@ -9,9 +9,10 @@ class RecargoController {
   def springSecurityService
 
   def nuevo(){
-    Recargo recargo = new Recargo(params)
-    recargo.organizacion = springSecurityService.currentUser.instituciones?.first()  
-    recargo.save(flush:true)
+    Recargo recargo = Recargo.get(params.recargoId ?: 0) ?: new Recargo(cantidad:params.cantidad,
+                                                                       porcentaje:params.porcentaje,
+                                                                       organizacion:springSecurityService.currentUser.instituciones?.first()).save(flush:true)
+
     render template:"/recargo/list", model:[recargos:recargo, recargoId:recargo.id]
   }
   
