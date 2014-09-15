48d55f5a3df114e2caade54a8308ffbd21231b7b
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-15 11:03:33 -0500

    coding #310 modificando Marshaller para los descuentos

--- src/groovy/com/stele/marshallers/DescuentoMarshaller.groovy
+++ src/groovy/com/stele/marshallers/DescuentoMarshaller.groovy
@@ -1,23 +1,24 @@
 package com.stele
 
 import grails.converters.JSON
-import com.payable.Descuento
+import com.payable.Discount
 import org.codehaus.groovy.grails.web.converters.marshaller.ObjectMarshaller
 
 public class DescuentoMarshaller implements ObjectMarshaller<JSON> {
 
   public boolean supports(Object object) {
-    return object instanceof Descuento;
+    return object instanceof Discount
   }
 
   public void marshalObject(Object object, JSON converter) {
-    def descuento = object as Descuento
-    def decuentoValues = [key:descuento.id,value:descuento.nombreDeDescuento,
-                          porcentaje: descuento.porcentaje,
-                          cantidad: descuento.cantidad,
-                          dias: descuento.diasPreviosParaCancelarDescuento]
+    def discount = object as Discount
+    def discountValues = [key:discount.id,
+                          value:discount.discountName,
+                          porcentaje: discount.percentage,
+                          cantidad: discount.amount,
+                          dias: discount.previousDaysForCancelingDiscount]
 
-    converter.convertAnother(decuentoValues)
+    converter.convertAnother(discountValues)
   }
 
 }
