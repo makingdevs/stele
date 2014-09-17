ecfd51aed7764f7ebc92d8267bc19f78f1f2d0cc
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-17 12:35:09 -0500

    coding #310 modificando wrapper service para que se ajuste con el nuevo plug-in

--- grails-app/services/com/stele/WrapperCommandService.groovy
+++ grails-app/services/com/stele/WrapperCommandService.groovy
@@ -1,15 +1,15 @@
 package com.stele
 
-import com.payable.GrupoPagoCommand
+import com.payable.PaymentGroupCommand
 import java.text.SimpleDateFormat
 
 class WrapperCommandService {
 
-  GrupoPagoCommand generarParseoDeCamadaPagoCommandAGrupoPagoCommand(CamadaPagoCommand camadaPagoCommand, Institucion institucion) {
-    GrupoPagoCommand gcp = new GrupoPagoCommand()
+  PaymentGroupCommand generarParseoDeCamadaPagoCommandAGrupoPagoCommand(CamadaPagoCommand camadaPagoCommand, Institucion institucion) {
+    PaymentGroupCommand pgc = new PaymentGroupCommand()
     if (camadaPagoCommand.recargoid){
       if (camadaPagoCommand.recargoid.first() != "")
-        gcp.recargoId = camadaPagoCommand.recargoid?.first()?.toLong()
+        pgc.surchargeId = camadaPagoCommand.recargoid?.first()?.toLong()
     }
     
     if(camadaPagoCommand.fechasDeExpiracionDescuento){
@@ -23,30 +23,30 @@ class WrapperCommandService {
           log.error "The date has not been converted"
         }
       }
-      gcp.fechasDeExpiracionDescuento = fechasDeExpiracionParaDescuentos
+      pgc.expirationDatesForDiscounts = fechasDeExpiracionParaDescuentos
     }
 
-    if (!gcp.recargoId)
-      gcp.recargoId = camadaPagoCommand?.idRecargo?.toLong()
-    gcp.cantidadDePago = camadaPagoCommand.cantidadDePago ?: camadaPagoCommand.cantidadDePagoRecurrente
-    gcp.conceptoDePago = camadaPagoCommand.conceptoDePago ?: camadaPagoCommand.conceptoDePagoRecurrente
+    if (!pgc.surchargeId)
+      pgc.surchargeId = camadaPagoCommand?.idRecargo?.toLong()
+    pgc.paymentAmount = camadaPagoCommand.cantidadDePago ?: camadaPagoCommand.cantidadDePagoRecurrente
+    pgc.paymentConcept = camadaPagoCommand.conceptoDePago ?: camadaPagoCommand.conceptoDePagoRecurrente
     if (camadaPagoCommand?.fechaDeVencimiento)  
-      gcp.fechaDeVencimiento = new Date().parse("dd/MM/yyyy", camadaPagoCommand?.fechaDeVencimiento)
+      pgc.dueDate = new Date().parse("dd/MM/yyyy", camadaPagoCommand?.fechaDeVencimiento)
     if (camadaPagoCommand.descuentos){
       if (camadaPagoCommand.descuentos.first() != "") 
-        gcp.descuentoIds = camadaPagoCommand?.descuentos.collect{it.replace('[','').replace(']','')}
+        pgc.discountIds = camadaPagoCommand?.descuentos.collect{it.replace('[','').replace(']','')}
     }
-    if (!gcp.descuentoIds)
-      gcp.descuentoIds = camadaPagoCommand.idsDescuentos?.replace('[','')?.replace(']','')?.split(',')
-    gcp.organizacion = institucion
-    gcp.diasVencimientoPago = camadaPagoCommand.diasVencimientoPago
-    gcp.payables = obtenerListaDePayables(camadaPagoCommand)
-    gcp.meses = camadaPagoCommand.meses ?: []
-    gcp.pagoDoble = camadaPagoCommand?.pagoDoble ?:[]
-    gcp
+    if (!pgc.discountIds)
+      pgc.discountIds= camadaPagoCommand.idsDescuentos?.replace('[','')?.replace(']','')?.split(',')
+    pgc.organization = institucion
+    pgc.daysPaymentDue = camadaPagoCommand.diasVencimientoPago
+    pgc.instances = obtenerListaDeDependientes(camadaPagoCommand)
+    pgc.months = camadaPagoCommand.meses ?: []
+    pgc.doublePayment = camadaPagoCommand?.pagoDoble ?:[]
+    pgc
   }
 
-  private def obtenerListaDePayables(CamadaPagoCommand camadaPagoCommand) {
+  private def obtenerListaDeDependientes(CamadaPagoCommand camadaPagoCommand) {
     def listaDependientesExistentes = []
     def dependientes = Dependiente.findAllByCamada(camadaPagoCommand.camada)
     listaDependientesExistentes << camadaPagoCommand?.listaDependientes?.replace('[','')?.replace(']','')?.split(',')
