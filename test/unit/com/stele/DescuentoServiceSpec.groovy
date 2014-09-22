290adc4e5dc2f2c4be5ce739b87efde9ba54fc84
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-22 13:19:12 -0500

    cambiando la clase de Descuento por Discount en las pruebas del servicio de Descuentos

--- test/unit/com/stele/DescuentoServiceSpec.groovy
+++ test/unit/com/stele/DescuentoServiceSpec.groovy
@@ -5,28 +5,28 @@ import spock.lang.Specification
 import com.makingdevs.*
 import com.payable.*
 
-@TestFor(DescuentoService)
-@Mock([Institucion, Descuento])
+@TestFor(DiscountService)
+@Mock([Institucion, Discount])
 class DescuentoServiceSpec extends Specification {
 
-  def "Obtener todos los descuentos ligados a una Institucion"() {
+  def "Obtener todos los descuentodescuentos ligados a una Institucion"() {
     given: 
       Institucion institucion = new Institucion()
-      institucion.nombre = "la chafita"
+      institucion.name = "la chafita"
       institucion.save(validate:false)
     and:
-      Descuento descuento = new Descuento()
-      descuento.nombreDeDescuento = "pruebaDescuentote"
-      descuento.organizacion = institucion
-      descuento.cantidad = 1000
-      descuento.save(validate:false)
-    when: "Se realiza la llamada al metodo buscarDescuentosDeUnaInstitucion"
+      Discount discount = new Discount()
+      discount.discountName = "pruebaDescuentote"
+      discount.organization = institucion
+      discount.amount = 1000
+      discount.save(validate:false)
+    when: "Se realiza la llamada al metodo buscardiscountsDeUnaInstitucion"
       def query = "esc"
-      def descuentoInstitucion = service.buscarDescuentosDeUnaOrganizacion(institucion, query)
-    then: "La cantidad de descuentos debe ser igual a 1"
+      def descuentoInstitucion = service.searchDiscountsOfAnOrganization(institucion, query)
+    then: "La cantidad de discounts debe ser igual a 1"
       assert descuentoInstitucion.size() == 1
-      assert descuentoInstitucion.first().nombreDeDescuento == "pruebaDescuentote"
-      assert descuentoInstitucion.first().cantidad == 1000
+      assert descuentoInstitucion.first().discountName == "pruebaDescuentote"
+      assert descuentoInstitucion.first().amount == 1000
   }
 
 }
