fe9e9c40fd315390c36f7ff4f1205ff4929d9958
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-22 13:50:53 -0500

    reemplazando clase Pago por Payment y agregando el PaymentLink

--- test/unit/com/stele/ReciboDePagoServiceSpec.groovy
+++ test/unit/com/stele/ReciboDePagoServiceSpec.groovy
@@ -4,22 +4,24 @@ import grails.text.mixin.*
 import org.junit.*
 import spock.lang.Specification
 import spock.lang.Unroll
-import com.payable.TipoDePago
-import com.payable.Pago
+import com.payable.PaymentType
+import com.payable.Payment
+import com.payable.PaymentLink
 import com.makingdevs.Perfil
 import com.stele.seguridad.Usuario
 
 @TestFor(ReciboDePagoService)
-@Mock([Pago,Dependiente,Perfil,Usuario,HistorialAcademico,DistribucionInstitucional])
+@Mock([Payment,PaymentLink,Dependiente,Perfil,Usuario,HistorialAcademico,DistribucionInstitucional])
 class ReciboDePagoServiceSpec extends Specification {
   
   def "Obtener datos del recibo de Pago"(){
     given:"Se tiene un pago conciliado de un dependiente"
       def fecha = new GregorianCalendar(2014, Calendar.MARCH, 11) 
-      def pagoConciliado = new Pago(conceptoDePago:"Inscripcion",cantidadDePago:900,recargosAcumulados:400,
-                                    fechaDePago:fecha.time,fechaDeVencimiento:fecha.time,
-                                    tipoDePago:TipoDePago.TRANSFERENCIA_BANCARIA,
-                                    referencia:"2000").save(validate:false);
+      def pagoConciliado = new Payment(paymentConcept:"Inscripcion",paymentAmount:900,accumulatedSurcharges:400,
+                                    paymentDate:fecha.time,fechaDeVencimiento:fecha.time,
+                                    paymentType:PaymentType.WIRE_TRANSFER,
+                                    reference:"2000").save(validate:false);
+
       def perfilTutor = new Perfil(nombre:"Silvia",
                                    apellidoPaterno:"Garcia",
                                    apellidoMaterno:"Campos").save(validate:false)
@@ -29,7 +31,8 @@ class ReciboDePagoServiceSpec extends Specification {
       def perfilAlumno = new Perfil(nombre:"Gamaliel",
                                     apellidoPaterno:"Jimenez",
                                     apellidoMaterno:"Garcia").save(validate:false)
-      def dependiente = new Dependiente(perfil:perfilAlumno,pagos:[pagoConciliado],usuario:usuario).save(validate:false)
+      def dependiente = new Dependiente(perfil:perfilAlumno,usuario:usuario).save(validate:false)
+      def paymentLink = new PaymentLink(paymentRef:dependiente.id,type:dependiente.class.simpleName,payments:[pagoConciliado]).save(validate:false)
       def distribucionInstitucional = new DistribucionInstitucional(nivelDeEstudio:NivelDeEstudio.PRIMARIA,grado:4,grupo:"A").save(validate:false)
       def historialAcademico = new HistorialAcademico(distribucionInstitucional:distribucionInstitucional,
                                                       dependiente:dependiente).save(validate:false)
@@ -56,7 +59,7 @@ class ReciboDePagoServiceSpec extends Specification {
       datosReciboDePago.recargosAcumulados == 400
       datosReciboDePago.total == 500
       datosReciboDePago.fechaPago == fecha.time
-      datosReciboDePago.tipoPago == TipoDePago.TRANSFERENCIA_BANCARIA.toString()
+      datosReciboDePago.tipoPago == PaymentType.WIRE_TRANSFER.toString()
       datosReciboDePago.referencia == "2000"
   }
 }
