d7d9ba81fee5c9c4b0c5e257bb6b48cf32c69c3f
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-24 10:21:44 -0500

    coding #315 actualizando los cambios a la base

--- grails-app/migrations/cambiando-extends-institucion.groovy
+++ grails-app/migrations/cambiando-extends-institucion.groovy
@@ -10,12 +10,41 @@ databaseChangeLog = {
 		}
 	}
 
-	changeSet(author: "makingdevs (generated)", id: "1410446070553-18") {
-		dropColumn(columnName: "class", tableName: "organizacion")
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-4") {
+    dropForeignKeyConstraint(baseTableName: "cuentas_bancarias", baseTableSchemaName: "qa_stele_db", constraintName: "FKD9D2BCC2418DE2A1")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-5") {
+    dropForeignKeyConstraint(baseTableName: "distribucion_institucional", baseTableSchemaName: "qa_stele_db", constraintName: "FK4B3D26CA8C0A9FFC")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-6") {
+    dropForeignKeyConstraint(baseTableName: "usuario_instituciones", baseTableSchemaName: "qa_stele_db", constraintName: "FKF5FF5E868C0A9FFC")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-18") {
+    dropColumn(columnName: "class", tableName: "organizacion")
 	}
 
   changeSet(author: "makingdevs (generated)", id: "1410446070554-1") {
     sql("update organization set class=\"${Institucion.class.name}\"") 
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-18") {
+    dropColumn(columnName: "class", tableName: "organizacion")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-7") {
+    addForeignKeyConstraint(baseColumnNames: "organizacion_id", baseTableName: "cuentas_bancarias", constraintName: "FKD9D2BCC2418DE2A1", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "organization", referencesUniqueColumn: "false")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-8") {
+    addForeignKeyConstraint(baseColumnNames: "institucion_id", baseTableName: "distribucion_institucional", constraintName: "FK4B3D26CA8C0A9FFC", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "organization", referencesUniqueColumn: "false")
+  }
+
+  changeSet(author: "makingdevs (generated)", id: "1410446070553-9") {
+    addForeignKeyConstraint(baseColumnNames: "direccion_id", baseTableName: "institucion_direccion", constraintName: "FK6F9EC3DAA649AEE2", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "direccion", referencesUniqueColumn: "false")
   }
 
 }
