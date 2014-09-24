acf1cfea9459ff5c88ebef803c954e0bd2b69120
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-09-24 12:09:08 -0500

    coding #315 cambiando a InnoDB

--- grails-app/migrations/agregando-foreign-keys.groovy
+++ grails-app/migrations/agregando-foreign-keys.groovy
@@ -1,6 +1,10 @@
 databaseChangeLog = {
 
-	changeSet(author: "makingdevs (generated)", id: "1411577238852-3") {
+  changeSet(author: "makingdevs (generated)", id:"1411577238852-2"){
+    sql("ALTER TABLE qa_stele_db.organization ENGINE=InnoDB")
+  }
+	
+  changeSet(author: "makingdevs (generated)", id: "1411577238852-3") {
 		addForeignKeyConstraint(baseColumnNames: "organizacion_id", baseTableName: "cuentas_bancarias", constraintName: "FKD9D2BCC2418DE2A1", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "id", referencedTableName: "organization", referencesUniqueColumn: "false")
 	}
 
