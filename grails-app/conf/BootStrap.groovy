dde0a211dfa8e026381fd00ae258aa3eb55d138b
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-10-03 16:02:05 -0500

    coding #324 agregando la creación de los roles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -16,8 +16,8 @@ class BootStrap {
 
   def init = { servletContext ->
     wrapperMelodyDataSource()
+    crearRoles()
     //creaInstituciones()
-    //creaUsuario()
     JSON.createNamedConfig('stele') {
       it.registerObjectMarshaller(new EsquemaDePagoMarshaller())
       it.registerObjectMarshaller(new ConceptoMarshaller())
@@ -46,61 +46,10 @@ class BootStrap {
   private def wrapperMelodyDataSource(){
     dataSource.targetDataSource = JdbcWrapper.SINGLETON.createDataSourceProxy(dataSource.targetDataSource)
   }
-/*  
-  private def creaInstituciones(){
-    new Institucion(nombre:"Instituto Juana de Arco").save(flush:true)
-    new Institucion(nombre:"Kinder Primeros pasitos").save(flush:true)
-    new Institucion(nombre:"Escuela Secundaria Coyoalhuiqui").save(flush:true)
-    new Institucion(nombre:"Instituto Andersen").save(flush:true)
-    new Institucion(nombre:"Colegio Americano").save(flush:true)
-    new Institucion(nombre:"Colegio Alemán").save(flush:true)
+
+  def crearRoles(){
+    def rolTutor = Rol.findByAuthority("ROLE_PADRE_TUTOR") ?: new Rol(authority:"ROLE_PADRE_TUTOR").save(flush:true)
+    def rolDirector = Rol.findByAuthority("ROLE_DIRECTOR") ?: new Rol(authority:"ROLE_DIRECTOR").save(flush:true)
   }
 
-  private void creaUsuario() {
-    def usuario = Usuario.findByUsername("nelson@muntz.com")
-    if(!usuario) {
-      Perfil perfil = new Perfil( nombre: "Nelson",
-                                  apellidoPaterno: "Muntz")
-      perfil.save(flush:true)
-      usuario = new Usuario( username:"nelson@muntz.com",
-                   password:"haha",
-                   enabled:true,
-                   accountExpired:false,
-                   accountLocked:false,
-                   passwordExpired:false,
-                   perfil: perfil)
-
-      usuario.save(flush:true)
-    }
-    def rol = Rol.findByAuthority("ROLE_PADRE_TUTOR")
-    if(!rol)
-      rol = new Rol(authority:"ROLE_PADRE_TUTOR").save(flush:true)
-    def rolDirector = Rol.findByAuthority("ROLE_DIRECTOR")
-    if(!rolDirector)
-      rolDirector = new Rol(authority:"ROLE_DIRECTOR").save(flush:true)
-    def usuarioRol = UsuarioRol.findByUsuarioAndRol(usuario,rol)
-    def user = Usuario.read(usuario.id)
-    if(!usuarioRol)
-      usuarioRol = UsuarioRol.create(user, rol, true)
-
-    Perfil perfilDependiente = new Perfil(nombre: "Nelson Jr.",
-                                          apellidoPaterno: "Muntz")
-    perfilDependiente.save(flush:true)
-    Dependiente dependiente = new Dependiente(matricula : "2013A9023",
-                                              perfil : perfilDependiente,
-                                              camada : "0000000001")
-    Pago pago = new Pago(conceptoDePago : "Pago test 1",
-                         cantidadDePago : 123456789,
-                         fechaDePago : new Date(),
-                         fechaDeVencimiento : new Date(),
-                         tipoDePago : TipoDePago.TRANSFERENCIA_BANCARIA,
-                         estaDePago : EstatusDePago.CREADO,
-                         transactionId : UUID.randomUUID().toString().replaceAll('-', '').substring(0,20)
-                         )
-    pago.save(flush:true)
-    dependiente.addToPagos( pago )
-    usuario.addToDependientes( dependiente )
-    usuario.save(flush:true)
-  }
-*/
 }
