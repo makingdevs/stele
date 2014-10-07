f1ba8516b39da052d264519199d38aa50ac3aff8
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-10-07 16:33:58 -0500

    coding #322 agregando una prueba para no registrar un dependiente con la misma matrícula en la misma institución

--- test/unit/com/stele/InscripcionManualServiceSpec.groovy
+++ test/unit/com/stele/InscripcionManualServiceSpec.groovy
@@ -2,7 +2,7 @@ package com.stele
 
 import grails.test.mixin.TestFor
 import grails.test.mixin.Mock
-import spock.lang.Specification
+import spock.lang.*
 import com.stele.seguridad.Usuario
 
 
@@ -10,42 +10,94 @@ import com.stele.seguridad.Usuario
 @Mock([Institucion,Usuario,Dependiente,CicloEscolar,DistribucionInstitucional,HistorialAcademico])
 class InscripcionManualServiceSpec extends Specification {
 
-    def "Registrar un Alumno y un tutor nuevo"() {
-      setup :
-        Institucion institucion = new Institucion()
-        institucion.name = institucionNombre
-        institucion.save(validate:false)
-      and : 
-        InscripcionCommand command = new InscripcionCommand(
-          cicloEscolar : cicloEscolar,
-          nombreAlumno : nombreAlumno,
-          apellidoPaternoAlumno : apellidoPaternoAlumno,
-          apellidoMaternoAlumno : apellidoMaternoAlumno,
-          nivelDeEstudio : nivelDeEstudio,
-          grado : grado,
-          grupo : grupo,
-          turno : turno,
-          matricula : matricula,
-          nombrePadre : nombrePadre,
-          apellidoPaternoPadre : apellidoPaternoPadre,
-          apellidoMaternoPadre : apellidoMaternoPadre,
-          telefono : telefono,
-          email : email)
-      and :
-        def mocks = crearColaboradores()
-      when :
-        def registro = service.generarRegistroDeAlumnoYTutor(command, institucion)
-        mocks*.verify()
-      then : 
-        assert registro instanceof LinkedHashMap
+  def "Registrar un Alumno y un tutor nuevo"() {
+    setup :
+      Institucion institucion = new Institucion()
+      institucion.name = institucionNombre
+      institucion.save(validate:false)
+    and : 
+      InscripcionCommand command = new InscripcionCommand(
+      cicloEscolar : cicloEscolar,
+      nombreAlumno : nombreAlumno,
+      apellidoPaternoAlumno : apellidoPaternoAlumno,
+      apellidoMaternoAlumno : apellidoMaternoAlumno,
+      nivelDeEstudio : nivelDeEstudio,
+      grado : grado,
+      grupo : grupo,
+      turno : turno,
+      matricula : matricula,
+      nombrePadre : nombrePadre,
+      apellidoPaternoPadre : apellidoPaternoPadre,
+      apellidoMaternoPadre : apellidoMaternoPadre,
+      telefono : telefono,
+      email : email)
+    and :
+      def mocks = crearColaboradores()
+    when :
+      def registro = service.generarRegistroDeAlumnoYTutor(command, institucion)
+      mocks*.verify()
+    then : 
+      assert registro instanceof LinkedHashMap
+    where :
+      cicloEscolar | nombreAlumno | apellidoPaternoAlumno | apellidoMaternoAlumno | nivelDeEstudio | grado | grupo | turno      | matricula | nombrePadre | apellidoPaternoPadre | apellidoMaternoPadre | telefono    | email           | institucionNombre
+      "2012/2013" | "sergio"     | "rodriguez"           | "duran"               | "SECUNDARIA"   | 3     | "b"   | "MATUTINO" | "M656565" | "Anegeles"  | "duran"              | "romero"             | "56565672"  | "angeles@m.com" | "Springfield Elementary school"
+  }
+ 
+  @Ignore
+  def "Registrar un alumno con una matrícula repetida en la misma institución"() {
+    given : "Un dependiente inscrito en la institución"
+      Institucion institucion = new Institucion()
+      institucion.name = institucionNombre
+      institucion.save(validate:false)
+      
+    and : 
+      InscripcionCommand command = new InscripcionCommand(cicloEscolar : cicloEscolar,
+                                                          nombreAlumno : nombreAlumno,
+                                                          apellidoPaternoAlumno : apellidoPaternoAlumno,
+                                                          apellidoMaternoAlumno : apellidoMaternoAlumno,
+                                                          nivelDeEstudio : nivelDeEstudio,
+                                                          grado : grado,
+                                                          grupo : grupo,
+                                                          turno : turno,
+                                                          matricula : matricula,
+                                                          nombrePadre : nombrePadre,
+                                                          apellidoPaternoPadre : apellidoPaternoPadre,
+                                                          apellidoMaternoPadre : apellidoMaternoPadre,
+                                                          telefono : telefono,
+                                                          email : email)
+    and :
+      Usuario user = new Usuario()
+      Dependiente dependiente = new Dependiente(camada:"Escuela_Superior_de_computo_${new Date()}")
+      DistribucionInstitucional distribucion = new DistribucionInstitucional()
+      CicloEscolar cicloEscolar = new CicloEscolar()
+      HistorialAcademico historialAcademico = new HistorialAcademico()
+      createMockForUserService()
+      
+    when :
+      def registro = service.generarRegistroDeAlumnoYTutor(command, institucion)
+      mocks*.verify()
 
-      where :
-        cicloEscolar | nombreAlumno | apellidoPaternoAlumno | apellidoMaternoAlumno | nivelDeEstudio | grado | grupo | turno      | matricula | nombrePadre | apellidoPaternoPadre | apellidoMaternoPadre | telefono    | email           | institucionNombre
-         "2012/2013" | "sergio"     | "rodriguez"           | "duran"               | "SECUNDARIA"   | 3     | "b"   | "MATUTINO" | "M656565" | "Anegeles"  | "duran"              | "romero"             | "56565672"  | "angeles@m.com" | "Springfield Elementary school"
+    then : 
+      assert registro instanceof LinkedHashMap
 
-    }
+    where :
+      cicloEscolar | nombreAlumno | apellidoPaternoAlumno | apellidoMaternoAlumno | nivelDeEstudio | grado | grupo | turno      | matricula | nombrePadre | apellidoPaternoPadre | apellidoMaternoPadre | telefono    | email           | institucionNombre
+      "2014/2015"  | "Gamaliel"   | "Jiménez"             | "García"              | "SECUNDARIA"   | 3     | "C"   | "MATUTINO" | "M656565" | "Anegeles"  | "duran"              | "romero"             | "56565672"  | "angeles@m.com" | "Springfield Elementary school"
+  }
 
-    private def crearColaboradores() {
+  def createMockForUserService(){
+    Usuario user = new Usuario()
+    def usuarioServiceMock = mockFor(UsuarioService) 
+    usuarioServiceMock.demand.obtenerUsuarioDesdeCommand(1..1){ obj -> return user}
+    usuarioServiceMock.demand.registrar(1..1){ obj, obj2 -> return user.save(validate:false)}
+    service.usuarioService = usuarioServiceMock.createMock() 
+  }
+
+  def creteMockForDependienteService(){
+    def dependienteServiceMock = mockFor(DependienteService)
+  }
+  
+  private def crearColaboradores() {
       Usuario user = new Usuario()
       Dependiente dependiente = new Dependiente(camada:"")
       DistribucionInstitucional distribucion = new DistribucionInstitucional()
