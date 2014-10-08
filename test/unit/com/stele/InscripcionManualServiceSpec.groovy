f2c172bb8554082a0429ae65c576141ef2ab5b7d
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-10-08 11:55:54 -0500

    coding #322 refactor a la prueba

--- test/unit/com/stele/InscripcionManualServiceSpec.groovy
+++ test/unit/com/stele/InscripcionManualServiceSpec.groovy
@@ -5,42 +5,62 @@ import grails.test.mixin.Mock
 import spock.lang.*
 import com.stele.seguridad.Usuario
 
-
 @TestFor(InscripcionManualService)
 @Mock([Institucion,Usuario,Dependiente,CicloEscolar,DistribucionInstitucional,HistorialAcademico])
 class InscripcionManualServiceSpec extends Specification {
 
   def "Registrar un Alumno y un tutor nuevo"() {
     setup :
-      Institucion institucion = new Institucion()
-      institucion.name = institucionNombre
-      institucion.save(validate:false)
+      Institucion institucion = new Institucion(name:"Springfield Elementary school").save(validate:false)
     and : 
-      InscripcionCommand command = new InscripcionCommand(
-      cicloEscolar : cicloEscolar,
-      nombreAlumno : nombreAlumno,
-      apellidoPaternoAlumno : apellidoPaternoAlumno,
-      apellidoMaternoAlumno : apellidoMaternoAlumno,
-      nivelDeEstudio : nivelDeEstudio,
-      grado : grado,
-      grupo : grupo,
-      turno : turno,
-      matricula : matricula,
-      nombrePadre : nombrePadre,
-      apellidoPaternoPadre : apellidoPaternoPadre,
-      apellidoMaternoPadre : apellidoMaternoPadre,
-      telefono : telefono,
-      email : email)
+      InscripcionCommand command = new InscripcionCommand(cicloEscolar : "2012/2013",
+                                                          nombreAlumno : "Sergio",
+                                                          apellidoPaternoAlumno : "Rodriguez",
+                                                          apellidoMaternoAlumno : "Duran",
+                                                          nivelDeEstudio : "SECUNDARIA",
+                                                          grado : 3,
+                                                          grupo : "B",
+                                                          turno : "MATUTINO",
+                                                          matricula : "M656565",
+                                                          nombrePadre : "Angeles",
+                                                          apellidoPaternoPadre : "Duran",
+                                                          apellidoMaternoPadre : "Romero",
+                                                          telefono : "56565672",
+                                                          email : "angeles@m.com")
     and :
-      def mocks = crearColaboradores()
+      Dependiente dependiente = new Dependiente(matricula:command.matricula)
+      Usuario usuario = new Usuario(username:"egjimenezg@gmail.com",
+                                    instituciones:[institucion])
+
+      def usuarioServiceMock = mockFor(UsuarioService)
+      def dependienteServiceMock = mockFor(DependienteService) 
+      def cicloEscolarServiceMock = mockFor(CicloEscolarService) 
+      def distribucionInstitucionalServiceMock = mockFor(DistribucionInstitucionalService)
+      def historialAcademicoServiceMock = mockFor(HistorialAcademicoService) 
+
+      usuarioServiceMock.demand.obtenerUsuarioDesdeCommand{InscripcionCommand cmd -> usuario} 
+      usuarioServiceMock.demand.registrar{ Usuario u, Institucion i -> usuario.save(validate:false)}
+      dependienteServiceMock.demand.obtenerDependienteDesdeCommand{InscripcionCommand cmd -> dependiente}
+      dependienteServiceMock.demand.registrar{ Dependiente d, Long userId, Institucion i -> return dependiente.save(validate:false) }
+      cicloEscolarServiceMock.demand.obtenerCicloEscolarDesdeCommand{obj -> return new CicloEscolar() }
+      distribucionInstitucionalServiceMock.demand.obtenerDistribucionInstitucionalDesdeCommand{ InscripcionCommand cmd -> new DistribucionInstitucional() }
+      
+      cicloEscolarServiceMock.demand.registrar{ new CicloEscolar().save(validate:false) }
+      distribucionInstitucionalServiceMock.demand.registrar{DistribucionInstitucional d, Long institucionId -> new DistribucionInstitucional().save(validate:false) }
+      historialAcademicoServiceMock.demand.preparaHistoricoAcademicoARegistrar{ Dependiente d, DistribucionInstitucional distribucionInstitucional -> new HistorialAcademico() }
+      historialAcademicoServiceMock.demand.registrar{ HistorialAcademico historialAcademico -> new HistorialAcademico()}
+
+      service.usuarioService = usuarioServiceMock.createMock()
+      service.dependienteService = dependienteServiceMock.createMock()
+      service.cicloEscolarService = cicloEscolarServiceMock.createMock()
+      service.distribucionInstitucionalService = distribucionInstitucionalServiceMock.createMock()
+      service.historialAcademicoService = historialAcademicoServiceMock.createMock()
+
     when :
       def registro = service.generarRegistroDeAlumnoYTutor(command, institucion)
-      mocks*.verify()
+      usuarioServiceMock.verify()
     then : 
-      assert registro instanceof LinkedHashMap
-    where :
-      cicloEscolar | nombreAlumno | apellidoPaternoAlumno | apellidoMaternoAlumno | nivelDeEstudio | grado | grupo | turno      | matricula | nombrePadre | apellidoPaternoPadre | apellidoMaternoPadre | telefono    | email           | institucionNombre
-      "2012/2013" | "sergio"     | "rodriguez"           | "duran"               | "SECUNDARIA"   | 3     | "b"   | "MATUTINO" | "M656565" | "Anegeles"  | "duran"              | "romero"             | "56565672"  | "angeles@m.com" | "Springfield Elementary school"
+      assert registro.message == "Se ha inscrito correctamente al dependiente" 
   }
  
   @Ignore
@@ -49,7 +69,6 @@ class InscripcionManualServiceSpec extends Specification {
       Institucion institucion = new Institucion()
       institucion.name = institucionNombre
       institucion.save(validate:false)
-      
     and : 
       InscripcionCommand command = new InscripcionCommand(cicloEscolar : cicloEscolar,
                                                           nombreAlumno : nombreAlumno,
@@ -67,16 +86,11 @@ class InscripcionManualServiceSpec extends Specification {
                                                           email : email)
     and :
       Usuario user = new Usuario()
-      Dependiente dependiente = new Dependiente(camada:"Escuela_Superior_de_computo_${new Date()}")
-      DistribucionInstitucional distribucion = new DistribucionInstitucional()
-      CicloEscolar cicloEscolar = new CicloEscolar()
-      HistorialAcademico historialAcademico = new HistorialAcademico()
       createMockForUserService()
       
     when :
       def registro = service.generarRegistroDeAlumnoYTutor(command, institucion)
       mocks*.verify()
-
     then : 
       assert registro instanceof LinkedHashMap
 
@@ -85,51 +99,5 @@ class InscripcionManualServiceSpec extends Specification {
       "2014/2015"  | "Gamaliel"   | "Jiménez"             | "García"              | "SECUNDARIA"   | 3     | "C"   | "MATUTINO" | "M656565" | "Anegeles"  | "duran"              | "romero"             | "56565672"  | "angeles@m.com" | "Springfield Elementary school"
   }
 
-  def createMockForUserService(){
-    Usuario user = new Usuario()
-    def usuarioServiceMock = mockFor(UsuarioService) 
-    usuarioServiceMock.demand.obtenerUsuarioDesdeCommand(1..1){ obj -> return user}
-    usuarioServiceMock.demand.registrar(1..1){ obj, obj2 -> return user.save(validate:false)}
-    service.usuarioService = usuarioServiceMock.createMock() 
-  }
-
-  def creteMockForDependienteService(){
-    def dependienteServiceMock = mockFor(DependienteService)
-  }
   
-  private def crearColaboradores() {
-      Usuario user = new Usuario()
-      Dependiente dependiente = new Dependiente(camada:"")
-      DistribucionInstitucional distribucion = new DistribucionInstitucional()
-      CicloEscolar cicloEscolar = new CicloEscolar()
-      HistorialAcademico historialAcademico = new HistorialAcademico()
-
-      def usuarioServiceMock = mockFor(UsuarioService)
-      usuarioServiceMock.demand.obtenerUsuarioDesdeCommand(1..1){ obj -> return user}
-      usuarioServiceMock.demand.registrar(1..1){ obj , obj2-> return user}
-      service.usuarioService  = usuarioServiceMock.createMock()
-
-      def dependienteServiceMock = mockFor(DependienteService)
-      dependienteServiceMock.demand.obtenerDependienteDesdeCommand(1..1){ obj -> return dependiente}
-      dependienteServiceMock.demand.registrar(1..1){ obj, obj2 -> return dependiente}
-      service.dependienteService = dependienteServiceMock.createMock()
-
-      def cicloEscolarMock = mockFor(CicloEscolarService)
-      cicloEscolarMock.demand.obtenerCicloEscolarDesdeCommand(1..1){ obj-> return cicloEscolar}
-      cicloEscolarMock.demand.registrar(1..1){-> return cicloEscolar}
-      service.cicloEscolarService = cicloEscolarMock.createMock()
-
-      def distribucionMock = mockFor(DistribucionInstitucionalService)
-      distribucionMock.demand.obtenerDistribucionInstitucionalDesdeCommand(1..1){obj -> return distribucion}
-      distribucionMock.demand.registrar(1..1){ obj, obj2 -> distribucion}
-      service.distribucionInstitucionalService = distribucionMock.createMock()    
-
-      def historialAcademicoMock = mockFor(HistorialAcademicoService)
-      historialAcademicoMock.demand.preparaHistoricoAcademicoARegistrar(1..1){ obj, obj2 -> return historialAcademico}
-      historialAcademicoMock.demand.registrar(1..1){ obj -> return historialAcademico}
-      service.historialAcademicoService = historialAcademicoMock.createMock()
-
-      [usuarioServiceMock, dependienteServiceMock, cicloEscolarMock, distribucionMock, historialAcademicoMock]
-    }
-
 }
