57621e9f4d7c847286a98752a06d469362b9d406
AlvaroSalas <alvarosalasmtz@gmail.com>
2014-10-16 01:12:38 -0500

    coding #337 se crea reporteAlumnosInscritos/index.js para integrar codigo java scrips adicional al la vista

---
+++ web-app/js/reporteAlumnosInscritos/index.js
@@ -0,0 +1,35 @@
+jQuery(function($) {
+	var oTable1 = $('#tablaReporteAlumnos').dataTable( {
+		"bAutoWidth": false,
+		"aoColumns": [
+		  null,
+		  { "bSortable": false },{ "bSortable": false },
+		  null, null,null, null,null,null, null, null, null				
+		  ],
+		"aaSorting": [],
+		   "language": {
+			    "sProcessing":     "Procesando...",
+			    "sLengthMenu":     "Mostrar _MENU_ registros",
+			    "sZeroRecords":    "No se encontraron resultados",
+			    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+			    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+			    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+			    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+			    "sInfoPostFix":    "",
+			    "sSearch":         "Buscar:",
+			    "sUrl":            "",
+			    "sInfoThousands":  ",",
+			    "sLoadingRecords": "Cargando...",
+			    "oPaginate": {
+			        "sFirst":    "Primero",
+			        "sLast":     "Último",
+			        "sNext":     "Siguiente",
+			        "sPrevious": "Anterior"
+					},
+		    "oAria": {
+		        "sSortAscending":  ": clic para ordenar la columna de manera ascendente",
+		        "sSortDescending": ": clic para ordenar la columna de manera descendente"
+		    }
+		}
+    } );
+});
