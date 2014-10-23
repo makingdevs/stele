6d609987c2b68c643cc476c33e61ee2fd12126dd
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-10-23 12:57:00 -0500

    coding #341 agregando función a descuento unitario

--- web-app/js/paraCamada/descuentoUnitario.js
+++ web-app/js/paraCamada/descuentoUnitario.js
@@ -1,12 +1,15 @@
 window.DescuentoUnitario = (function(){
 
   DescuentoUnitario.prototype.fechaExpiracion = '';
+  DescuentoUnitario.prototype.formulario = '';
   DescuentoUnitario.prototype.operacionDescuento = '';
  
   function DescuentoUnitario(selectores,operacionDescuento){
-    this.operacionDescuento = operacionDescuento;
     this.fechaExpiracion = selectores.fechaExpiracion;
+    this.formulario = selectores.formulario; 
+    this.operacionDescuento = operacionDescuento;
     this.initDatePickerForFechaExpiracion();
+    this.initFormAction();
   }
 
   DescuentoUnitario.prototype.initDatePickerForFechaExpiracion = function(){
@@ -17,6 +20,30 @@ window.DescuentoUnitario = (function(){
       orientation: "top auto",
       autoclose:true
     });
+  }
+
+  DescuentoUnitario.prototype.initFormAction = function(){
+    var that = this;
+    this.formulario.submit(function(event){
+      event.stopPropagation();
+      $.ajax({
+        type: "POST",
+        url:$(this).attr("action"),
+        data:$(this).serialize()+"&referenceDate="+$("#fechaDeVencimiento").val(),
+        success: function(data){
+          $(".descuentoCreado").html(data);
+          that.formulario.each(function(){
+            this.reset();
+          }); 
+        }  
+      }).then(function(){
+        var discounts = $(".descuentoCreado input[name=discount]");
+        $(".descuentosIdDiv").html(discounts);
+      });
+
+      return false
+
+    }); 
   }
 
   return DescuentoUnitario;
