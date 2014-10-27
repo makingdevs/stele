3278b855f5a2dee5f0c3348a5c5e33a580bf1b7c
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-10-27 16:24:36 -0600

    coding #344 agregando mocks para los servicios utilizados

--- test/unit/com/stele/CobroParaCamadaServiceSpec.groovy
+++ test/unit/com/stele/CobroParaCamadaServiceSpec.groovy
@@ -10,14 +10,20 @@ class CobroParaCamadaServiceSpec extends Specification {
   
   def "Obtener pagos con descuentos aplicables"(){
     given:"una lista de pagos y un esquema de pagos con descuentos"
-      def paymentGroupCommand = new PaymentGroupCommand()
+      def paymentGroupCommand = new PaymentGroupCommand(expirationDatesForDiscounts:[new Date()+5])
       def paymentsList = [new Payment(dueDate:new Date()+10).save(validate:false)]
       def discount = new Discount(discountName:"Por pronto pago").save(validate:false)
       def paymentScheme = new PaymentScheme(paymentAmount:500)
       paymentScheme.addToDiscounts(discount)
       paymentScheme.save(validate:false)
+
+    and:
+      def applicableDiscountServiceMock = mockFor(ApplicableDiscountService) 
+      applicableDiscountServiceMock.demand.generateApplicableDiscountsForPaymentWithPaymentSchemeAndReferenceDate(1..1){ -> new ApplicableDiscount()  }
+      service.applicableDiscountService = applicableDiscountServiceMock.createMock()
     when:
       def pagos = service.obtenerPagosConDescuentosAplicablesSiTienenFechaDeVencimiento(paymentGroupCommand, paymentScheme, paymentsList) 
+      applicableDiscountServiceMock.verify()
     then:  
       pagos.size() == 1 
   }
