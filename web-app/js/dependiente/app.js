0dee2a28179b3e225002b657cae377d22d75d000
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-11-03 12:23:07 -0600

    coding #292 agregando el objeto de la clase dependiente e inyectandolo a la clase busqueda

--- web-app/js/dependiente/app.js
+++ web-app/js/dependiente/app.js
@@ -3,7 +3,6 @@ jQuery(function ($) {
 
   window.App = {
     init : function(){
-      this.initListDependiente(); 
       this.initBusqueda();
     },
     initBusqueda : function(){
@@ -11,17 +10,12 @@ jQuery(function ($) {
         turnoSelector: $("#turno"),
         nivelSelector:$("#nivel"),
         gradoSelector:$("#grado"),
-        grupoSelector:$("#grupo")
+        grupoSelector:$("#grupo"),
+        formularioSelector:$("#busquedaDependientes")
       };
       this.busqueda = new Busqueda(selectores);
-    },
-    initListDependiente : function(){
-      $("body").on("click","input[name=allDependientes]",function(){
-        if($(this).is(':checked'))
-          $("input[name=dependientes]").prop("checked",true);
-        else
-          $("input[name=dependientes]").prop("checked",false);
-      }); 
+      this.dependiente = new Dependiente();
+      this.busqueda.setDependiente(this.dependiente)
     }
   };
   App.init();
