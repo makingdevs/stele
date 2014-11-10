64e3c128895ded658a9a5fa855047a5754093556
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-11-10 17:03:25 -0600

    coding #355 mostrando pagos del tutor

--- grails-app/controllers/com/stele/EstadoDeCuentaController.groovy
+++ grails-app/controllers/com/stele/EstadoDeCuentaController.groovy
@@ -8,45 +8,40 @@ class EstadoDeCuentaController {
 
   def springSecurityService
   def cuentasBancariasService
+  def paymentService 
+  def accountingService
   def pagoService
 
   def show() {
     def dependiente
     def pagosDependiente
     def usuarioActual = springSecurityService.currentUser
-    def estatusDeCuenta = pagoService.estadoDeCuentaUsuario(usuarioActual)
     def dependientes = Dependiente.findAllByUsuario(usuarioActual)
     if (dependientes) {
+      
       if (params.idDependiente) 
         dependiente = Dependiente.findById(params.idDependiente)
       else
-        dependiente = Dependiente.findById(dependientes.first().id)      
-      pagosDependiente = dependiente.pagos
+        dependiente = Dependiente.findById(dependientes.first().id)
+      
+      pagosDependiente = accountingService.createStatementForInstances([dependiente])
+
     }    
     def cuentasBancariasExistentes = cuentasBancariasService.obtenerCuentasExistentesPorInstitucion(usuarioActual?.instituciones?.first())
     [
      usuarioActual : usuarioActual,
      listDependiente : dependientes,
-     pagosVencido : separarPagosEstadoDeCuenta(estatusDeCuenta.pagosVencidos, pagosDependiente),
-     pagosEnTiempo : separarPagosEstadoDeCuenta(estatusDeCuenta.pagosEnTiempo, pagosDependiente),
-     pagosPorRealizar : separarPagosPorMeses(separarPagosEstadoDeCuenta(estatusDeCuenta.pagosPorRealizar, pagosDependiente)),
-     pagoMensual : separarPagosPorMeses(separarPagosEstadoDeCuenta(estatusDeCuenta.pagoMensual, pagosDependiente)),
-     pagosRechazados : separarPagosEstadoDeCuenta(estatusDeCuenta.pagosRechazados, pagosDependiente),
-     pagosProcesados : separarPagosEstadoDeCuenta(estatusDeCuenta.pagosProcesados, pagosDependiente),
-     pagoCorrectos : separarPagosEstadoDeCuenta(estatusDeCuenta.pagoCorrectos, pagosDependiente),
+     pagosVencido : pagosDependiente.paymentsExpired, 
+     pagosEnTiempo : pagosDependiente.paymentsOnTime,
+     pagosPorRealizar : separarPagosPorMeses(pagosDependiente.paymentsToBeMade), 
+     pagoMensual :  separarPagosPorMeses(pagosDependiente.monthlyPayments), 
+     pagosRechazados : pagosDependiente.rejectedPayments,   
+     pagosProcesados : pagosDependiente.paymentsInProcess, 
+     pagoCorrectos : pagosDependiente.correctPayments, 
      electronica : cuentasBancariasExistentes.electronica,
      cheque : cuentasBancariasExistentes.cheque,
      ficha : cuentasBancariasExistentes.ficha
     ]
-  }
-
-  def separarPagosEstadoDeCuenta(def estadodeCuenta, def pagosDependiente) {
-    def pagosSeparados = []
-    estadodeCuenta.each{ pago -> 
-      if (pagosDependiente.contains(pago))
-        pagosSeparados.add(pago) 
-    }
-    pagosSeparados
   }
 
   def separarPagosPorMeses(def pagos) {
@@ -55,9 +50,9 @@ class EstadoDeCuentaController {
     def mesActual = obtenerNumeroDeMes(new Date()) + 1
     pagos.each{ pago ->
       def pagosList = []
-      def month = obtenerNumeroDeMes(pago.fechaDeVencimiento)
+      def month = obtenerNumeroDeMes(pago.dueDate)
         listaPagos.each{ v ->
-          def mes = obtenerNumeroDeMes(v.fechaDeVencimiento)
+          def mes = obtenerNumeroDeMes(v.dueDate)
           if (month == mes)
             pagosList.add(v)
         }
@@ -94,7 +89,7 @@ class EstadoDeCuentaController {
               pagosRechazados:payments.findAll { payment -> payment.paymentStatus == PaymentStatus.REJECTED }, 
               pagosEnTiempo:payments.findAll { payment -> payment.paymentStatus == PaymentStatus.CREATED },
               pagosPorRealizar:[]]
-    }
+  }
 
 
 }
