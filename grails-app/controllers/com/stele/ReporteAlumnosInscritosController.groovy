fdf25a94d03256031fecb18e383aaa6269462839
AlvaroSalas <alvarosalasmtz@gmail.com>
2014-11-13 01:17:43 -0600

    coding #360 se agrego metodo a ReporteAlumnosInscritosController para generar reporte en pdf de Alumnos Inscritos

--- grails-app/controllers/com/stele/ReporteAlumnosInscritosController.groovy
+++ grails-app/controllers/com/stele/ReporteAlumnosInscritosController.groovy
@@ -3,6 +3,8 @@ package com.stele;
 import org.codehaus.groovy.grails.plugins.jasper.JasperReportDef
 import org.codehaus.groovy.grails.plugins.jasper.JasperExportFormat
 
+import com.sun.org.apache.xalan.internal.xsltc.compiler.ForEach;
+
 class ReporteAlumnosInscritosController {
 	
 	def springSecurityService
@@ -15,13 +17,16 @@ class ReporteAlumnosInscritosController {
 		def listaGrados = reporteAlumnosInscritosService.obtenerGradosPorInstitrucion(user.instituciones[0])
 		def listaNiveles = reporteAlumnosInscritosService.obtenerNivelesDeEstudioPorInstitrucion(user.instituciones[0])
 		def listaTurnos = reporteAlumnosInscritosService.obtenerTurnosPorInstitrucion(user.instituciones[0])
-		def dependientes = reporteAlumnosInscritosService.obtenerAlumnosInscritosPorNivelGradoGrupoYTurno(
-			params.nivel != null && params.nivel.length() > 0 ? [(NivelDeEstudio)params.nivel] : listaNiveles, 
-			params.grado != null && params.grado.length() > 0 ? [params.grado.toInteger()] : listaGrados, 
-			params.grupo != null && params.grupo.length() > 0 ? [params.grupo] : listaGrupos,
-			params.turno != null && params.turno.length() > 0 ? [(Turno)params.turno] : listaTurnos,
-			user.instituciones[0])
-		return [params:params, listaGrupos:listaGrupos, listaGrados:listaGrados, dependientes:dependientes, listaNiveles:listaNiveles, listaTurnos:listaTurnos]
+		if(listaGrupos && listaGrados && listaNiveles && listaTurnos) {
+			def dependientes = reporteAlumnosInscritosService.obtenerAlumnosInscritosPorNivelGradoGrupoYTurno(
+				params.nivel != null && params.nivel.length() > 0 ? [(NivelDeEstudio)params.nivel] : listaNiveles, 
+				params.grado != null && params.grado.length() > 0 ? [params.grado.toInteger()] : listaGrados, 
+				params.grupo != null && params.grupo.length() > 0 ? [params.grupo] : listaGrupos,
+				params.turno != null && params.turno.length() > 0 ? [(Turno)params.turno] : listaTurnos,
+				user.instituciones[0])
+		
+			return [params:params, listaGrupos:listaGrupos, listaGrados:listaGrados, dependientes:dependientes, listaNiveles:listaNiveles, listaTurnos:listaTurnos]
+		} else flash.error = "No se encontraron registros de alumnos."
 	}
 	
 	def generaReporte() {
@@ -46,16 +51,77 @@ class ReporteAlumnosInscritosController {
                               nivel:it.historialAcademico.distribucionInstitucional.nivelDeEstudio[0],
                               grado:it.historialAcademico.distribucionInstitucional.grado[0],
                               grupo:it.historialAcademico.distribucionInstitucional.grupo[0],
-                              turno:it.historialAcademico.distribucionInstitucional.turno[0],
-                              cobros:"2,000.00",
-                              pagos:"2,000.00",
-                              saldo:"0.00"])
+                              turno:it.historialAcademico.distribucionInstitucional.turno[0]])
 		}
 		def report = new JasperReportDef(name:"reporteAlumnosInscritos.jasper",fileFormat:JasperExportFormat.CSV_FORMAT,
 										 reportData:listaAlumnos,
 										 locale:Locale.getDefault())
 		response.setContentType("application/csv")
 		response.setHeader "Content-disposition", "attachment; filename=" + user.instituciones[0].name.replaceAll(" ", "_") + "_alumnosInscritos.csv";
+		response.outputStream << jasperService.generateReport(report).toByteArray()
+	}
+	
+	def generaReportePdf() {
+		def listaDependientes = [];
+		def user = springSecurityService.currentUser
+		def listaGrupos = reporteAlumnosInscritosService.obtenerGruposPorInstitrucion(user.instituciones[0])
+		def listaGrados = reporteAlumnosInscritosService.obtenerGradosPorInstitrucion(user.instituciones[0])
+		def listaNiveles = reporteAlumnosInscritosService.obtenerNivelesDeEstudioPorInstitrucion(user.instituciones[0])
+		def listaTurnos = reporteAlumnosInscritosService.obtenerTurnosPorInstitrucion(user.instituciones[0])
+		def dependientes = reporteAlumnosInscritosService.obtenerAlumnosInscritosPorNivelGradoGrupoYTurno(
+			params.nivel != null && params.nivel.length() > 0 ? [(NivelDeEstudio)params.nivel] : listaNiveles,
+			params.grado != null && params.grado.length() > 0 ? [params.grado.toInteger()] : listaGrados,
+			params.grupo != null && params.grupo.length() > 0 ? [params.grupo] : listaGrupos,
+			params.turno != null && params.turno.length() > 0 ? [(Turno)params.turno] : listaTurnos,
+			user.instituciones[0])
+		if(params.nivel != null && params.nivel.length() > 0) {
+			def listaAlumnos = [];
+			def num = 1
+			dependientes.each {
+				listaAlumnos.add([no:num++,
+								  nombrePadreOTutor:it.usuario.perfil.nombre + it.usuario.perfil.apellidoPaterno + it.usuario.perfil.apellidoMaterno,
+								  correo:it.usuario.username,
+								  telefono:it.usuario.perfil.telefonos.numeroTelefonico[0],
+								  matricula:it.matricula,
+								  nombreAlumno:it.perfil.nombre + it.perfil.apellidoPaterno + it.perfil.apellidoMaterno,
+								  grado:it.historialAcademico.distribucionInstitucional.grado[0],
+								  grupo:it.historialAcademico.distribucionInstitucional.grupo[0],
+								  turno:it.historialAcademico.distribucionInstitucional.turno[0]])
+			}
+			listaDependientes.add([nivel: (NivelDeEstudio)params.nivel,
+								  totalNivel: listaAlumnos.size(),
+								  listaAlumnos:listaAlumnos])
+		} else {
+			listaNiveles.each {nivel ->
+				def listaAlumnos = [];
+				def num = 1
+				dependientes.each {
+					if(it.historialAcademico.distribucionInstitucional.nivelDeEstudio[0] == nivel) {
+						listaAlumnos.add([no:num++,
+										  nombrePadreOTutor:it.usuario.perfil.nombre + it.usuario.perfil.apellidoPaterno + it.usuario.perfil.apellidoMaterno,
+										  correo:it.usuario.username,
+										  telefono:it.usuario.perfil.telefonos.numeroTelefonico[0],
+										  matricula:it.matricula,
+										  nombreAlumno:it.perfil.nombre + it.perfil.apellidoPaterno + it.perfil.apellidoMaterno,
+										  grado:it.historialAcademico.distribucionInstitucional.grado[0],
+										  grupo:it.historialAcademico.distribucionInstitucional.grupo[0],
+										  turno:it.historialAcademico.distribucionInstitucional.turno[0]])
+					}
+				}
+				listaDependientes.add([nivel: nivel,
+									  totalNivel: listaAlumnos.size(),
+									  listaAlumnos:listaAlumnos])
+			}
+		}
+		def report = new JasperReportDef(name:"reporteAlumnosInscritosPdf.jasper",fileFormat:JasperExportFormat.PDF_FORMAT,
+										 reportData:listaDependientes,
+										 locale:Locale.getDefault(), 
+										 parameters:[nombreInstitucion:user.instituciones[0].name, 
+											 		 fechaEmision:new Date(), 
+													 totalAlumnos:dependientes.size(), 
+													 SUBREPORT_DIR:"${servletContext.getRealPath('/reports')}/"])
+		response.setContentType("application/pdf")
+		response.setHeader "Content-disposition", "attachment; filename=" + user.instituciones[0].name.replaceAll(" ", "_") + "_alumnosInscritos.pdf";
 		response.outputStream << jasperService.generateReport(report).toByteArray()
 	}
 }
